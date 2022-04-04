48f3971c0f918a65818be00e4a8e4b92f2dcf423
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-04 17:15:07 -0400

    Fix: Arreglado pequeño error en el modelo de los elementos.

--- model/entity/models.py
+++ model/entity/models.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 
 
 class Item(Base):
-    __table__ = 'items'
+    __tablename__ = 'items'
     id = Column(Integer, primary_key=True)
     name = Column(String(length=80), nullable=False, unique=True)
     description = Column(String(length=300), nullable=True, default='')
