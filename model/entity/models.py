6647b9b168d0bfe5db910532000b7c6a426e248a
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-04 15:41:46 -0400

    Feat: Creado el modelo de los elementos.

---
+++ model/entity/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+Base = declarative_base()
+
+
+class Item(Base):
+    __table__ = 'items'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(length=80), nullable=False, unique=True)
+    description = Column(String(length=300), nullable=True, default='')
