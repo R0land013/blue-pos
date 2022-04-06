7a1cff26cd04243545dc92801f3b0af92044c870
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-06 19:56:29 -0400

    Feat: Agregado método de igualdad y de conversión a cadena a los elementos generados.

--- tests/util/generators/item.py
+++ tests/util/generators/item.py
@@ -5,6 +5,14 @@ from faker import Faker
 fake = Faker()
 
 
+def equals(self, other: Item):
+    return self.name == other.name and self.description == other.description
+
+
+def to_string(self):
+    return '[name: "{}", description: "{}"]'.format(self.name, self.description)
+
+
 class ItemGenerator:
 
     @staticmethod
@@ -15,6 +23,8 @@ class ItemGenerator:
 
             a_item.name = fake.company()
             a_item.description = fake.text()
+            a_item.__eq__ = equals
+            a_item.__str__ = to_string
 
             items.append(a_item)
         return items
