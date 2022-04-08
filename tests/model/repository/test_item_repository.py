72e29e76958d01d5866d4cf9dab640c732c88787
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-08 19:43:06 -0400

    Feat, Fix: Creado test para probar que un error es levantado si se inserta un elemento con un nombre que ya ha sido asignado.
    	- Fix: Se debía hacer un commit para que la tabla de elementos fuera
    	vaciada correctamente, en el método tearDown de los tests de ItemRepository.

--- tests/model/repository/test_item_repository.py
+++ tests/model/repository/test_item_repository.py
@@ -1,6 +1,7 @@
 import unittest
 from sqlalchemy import select
 from model.entity.models import Item
+from model.repository.exc.item import UniqueItemNameException
 from model.repository.factory import RepositoryFactory
 from tests.util.generators.item import ItemGenerator
 from tests.util.general import TEST_DB_URL
@@ -16,6 +17,7 @@ class TestItemRepository(unittest.TestCase):
             statement = select(Item)
             for a_item in session.scalars(statement):
                 session.delete(a_item)
+            session.commit()
 
     def test_items_are_inserted_successfully(self):
         fake_items = ItemGenerator.generate_items_by_quantity(3)
@@ -27,3 +29,11 @@ class TestItemRepository(unittest.TestCase):
         with create_test_session() as session:
             inserted_items = session.scalars(select(Item)).all()
             self.assertEqual(inserted_items, fake_items)
+
+    def test_item_inserted_with_used_name_raise_exception(self):
+        fake_item = ItemGenerator.generate_one_item()
+        item_repository = RepositoryFactory.get_item_repository(TEST_DB_URL)
+
+        item_repository.insert_item(fake_item)
+
+        self.assertRaises(UniqueItemNameException, item_repository.insert_item, fake_item)
