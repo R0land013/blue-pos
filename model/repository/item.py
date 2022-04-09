61ce98e9cc6a6b789245c0e43ba4b656304ac0c8
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-08 20:03:35 -0400

    Feat: Cambios necesarios en insert_item de ItemRepository para lanzar UniqueItemNameException.
    	- Si se intenta insertar un elemento con un nombre que se encuentra en uso,
    	entonces se levantará una error.
    
    	- El test que verifica este comportamiento se ejecutó satisfactoriamente.

--- model/repository/item.py
+++ model/repository/item.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
-
 from model.entity.models import Item
+from sqlalchemy import select
+
+from model.repository.exc.item import UniqueItemNameException
 
 
 class ItemRepository:
@@ -9,8 +11,20 @@ class ItemRepository:
         self.session = session
 
     def insert_item(self, item: Item):
+        self.check_name_is_not_used(item)
         self.session.add(item)
         self.session.commit()
+
+    def check_name_is_not_used(self, item: Item):
+        found_item = self.find_item_by_name(item.name)
+        if found_item is not None:
+            raise UniqueItemNameException(found_item.name)
+
+    def find_item_by_name(self, name: str) -> Item:
+        return self.session.scalar(
+            select(Item)
+            .where(Item.name.ilike(name))
+        )
 
     def delete_item(self, item: Item):
         raise NotImplementedError()
