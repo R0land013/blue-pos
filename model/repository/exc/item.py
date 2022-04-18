d2789066d83433b3a52ba5d85f8518418c799daa
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-18 13:07:08 -0400

    Feat: Agregado método para obtener el nombre del elemento que levanta una excepción cuando se inserta con un nombre existente.

--- model/repository/exc/item.py
+++ model/repository/exc/item.py
@@ -4,4 +4,8 @@ class UniqueItemNameException(Exception):
     MSG = 'The item with name "{}" already exists.'
 
     def __init__(self, existing_name: str):
+        self.__name = existing_name
         super().__init__(UniqueItemNameException.MSG.format(existing_name))
+
+    def get_item_name(self):
+        return self.__name
