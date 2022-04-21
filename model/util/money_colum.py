72fd2173f181e35a72c155b9af52744b3a190b7f
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-21 17:57:16 -0400

    Feat: Creada clase para columna de tabla que representa dinero en el ORM.
    	- Esta clase es utilizada para convertir entre el tipo de dato
    	de la base de datos y el tipo de dato de Python. En este caso
    	se uso la clase Money del paquete money de pypi, versión 1.3.0.

---
+++ model/util/money_colum.py
@@ -0,0 +1,21 @@
+from sqlalchemy import TypeDecorator, Text
+from money import Money
+
+
+class MoneyColumn(TypeDecorator):
+
+    impl = Text
+
+    cache_ok = True
+
+    def process_bind_param(self, money: Money, dialect):
+        return '{} {}'.format(money.currency, money.amount)
+
+    def process_result_value(self, money_as_str: str, dialect) -> Money:
+        money_as_list = money_as_str.split()
+        currency = money_as_list[0]
+        amount = money_as_list[1]
+        return Money(amount, currency)
+
+    def copy(self, **kw):
+        return MoneyColumn()
