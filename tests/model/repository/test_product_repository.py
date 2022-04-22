36aae81fa5bd523f009bdf003ce19ede8bfa715c
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-22 16:48:10 -0400

    Refactor: Cambios realizados a los tests del repositorio de productos. Los cambios fueron extracciones de funciones.

--- tests/model/repository/test_product_repository.py
+++ tests/model/repository/test_product_repository.py
@@ -1,14 +1,11 @@
 import unittest
-
-from sqlalchemy import select
-from model.entity.models import Product
 from model.repository.exc.product import UniqueProductNameException, NonExistentProductException, \
     InvalidProductQuantityException, InvalidPriceForProductException, NegativeProfitForProductException
 from model.repository.factory import RepositoryFactory
 from model.util.monetary_types import CUPMoney
 from tests.util.generators.product import ProductGenerator
-from tests.util.general import TEST_DB_URL
-from tests.util.general import create_test_session
+from tests.util.general import TEST_DB_URL, get_all_products_in_database, insert_product_and_return_it, \
+    get_one_product_from_database, insert_products_in_database, delete_all_products_from_database
 
 
 class TestProductRepository(unittest.TestCase):
@@ -18,22 +15,16 @@ class TestProductRepository(unittest.TestCase):
 
     def tearDown(self):
         RepositoryFactory.close_session()
-
-        with create_test_session() as session:
-            statement = select(Product)
-            for a_product in session.scalars(statement):
-                session.delete(a_product)
-            session.commit()
+        delete_all_products_from_database()
 
     def test_products_are_inserted_successfully(self):
         products = ProductGenerator.generate_products_by_quantity(3)
 
         for a_product in products:
             self.product_repository.insert_product(a_product)
+        inserted_products = get_all_products_in_database()
 
-        with create_test_session() as session:
-            inserted_products = session.scalars(select(Product)).all()
-            self.assertEqual(inserted_products, products)
+        self.assertEqual(inserted_products, products)
 
     def test_product_inserted_with_used_name_raise_exception(self):
         product = ProductGenerator.generate_one_product()
@@ -80,16 +71,12 @@ class TestProductRepository(unittest.TestCase):
 
     def test_product_is_deleted_successfully(self):
         product = ProductGenerator.generate_one_product()
-        with create_test_session() as session:
-            session.add(product)
-            session.commit()
+        product = insert_product_and_return_it(product)
 
-        with create_test_session() as session:
-            product = session.scalar(select(Product))
-            self.product_repository.delete_product(product)
+        self.product_repository.delete_product(product)
 
-            products = session.scalars(select(Product)).all()
-            self.assertEqual(len(products), 0)
+        products = get_all_products_in_database()
+        self.assertEqual(len(products), 0)
 
     def test_trying_to_delete_nonexistent_product_raises_exception(self):
         product = ProductGenerator.generate_one_product()
@@ -101,17 +88,14 @@ class TestProductRepository(unittest.TestCase):
     def test_product_is_updated_successfully(self):
         product = ProductGenerator.generate_one_product()
 
-        with create_test_session() as session:
-            session.add(product)
-            session.commit()
-            new_product = ProductGenerator.generate_one_product()
-            new_product.id = product.id
+        product = insert_product_and_return_it(product)
+        new_product = ProductGenerator.generate_one_product()
+        new_product.id = product.id
 
-            self.product_repository.update_product(new_product)
+        self.product_repository.update_product(new_product)
 
-            session.expunge_all()
-            updated_product = session.scalar(select(Product))
-            self.assertEqual(updated_product, new_product)
+        updated_product = get_one_product_from_database()
+        self.assertEqual(updated_product, new_product)
 
     def test_trying_to_update_nonexistent_product_raises_exception(self):
         product = ProductGenerator.generate_one_product()
@@ -125,76 +109,59 @@ class TestProductRepository(unittest.TestCase):
 
     def test_trying_to_update_product_using_existent_name_raises_exception(self):
         first_product, second_product = ProductGenerator.generate_products_by_quantity(2)
+        first_product = insert_product_and_return_it(first_product)
+        second_product = insert_product_and_return_it(second_product)
 
-        with create_test_session() as session:
-            session.add_all([first_product, second_product])
-            session.commit()
+        new_product = ProductGenerator.generate_one_product()
+        new_product.id = first_product.id
+        new_product.name = second_product.name
 
-            new_product = ProductGenerator.generate_one_product()
-            new_product.id = first_product.id
-            new_product.name = second_product.name
-
-            self.assertRaises(UniqueProductNameException, self.product_repository.update_product,
-                              new_product)
+        self.assertRaises(UniqueProductNameException, self.product_repository.update_product,
+                          new_product)
 
     def test_trying_to_update_product_using_same_name_does_not_raise_exception(self):
         product = ProductGenerator.generate_one_product()
+        product = insert_product_and_return_it(product)
+        new_product = ProductGenerator.generate_one_product()
+        new_product.id = product.id
+        new_product.name = product.name
 
-        with create_test_session() as session:
-            session.add(product)
-            session.commit()
-            new_product = ProductGenerator.generate_one_product()
-            new_product.id = product.id
-            new_product.name = product.name
+        self.product_repository.update_product(new_product)
 
-            self.product_repository.update_product(new_product)
-
-            session.expunge_all()
-            updated_product = session.scalar(select(Product))
-            self.assertEqual(updated_product, new_product)
+        updated_product = get_one_product_from_database()
+        self.assertEqual(updated_product, new_product)
 
     def test_product_updated_with_negative_quantity_raises_exception(self):
         product = ProductGenerator.generate_one_product()
+        product = insert_product_and_return_it(product)
         product.quantity = -1
-        with create_test_session() as session:
-            session.add(product)
-            session.commit()
 
-            self.assertRaises(InvalidProductQuantityException, self.product_repository.update_product,
-                              product)
+        self.assertRaises(InvalidProductQuantityException, self.product_repository.update_product,
+                          product)
 
     def test_product_updated_with_negative_price_raises_exception(self):
         product = ProductGenerator.generate_one_product()
+        product = insert_product_and_return_it(product)
+        product.price = CUPMoney('-1.00')
 
-        with create_test_session() as session:
-            session.add(product)
-            session.commit()
-
-            product.price = CUPMoney('-1.00')
-            self.assertRaises(InvalidPriceForProductException, self.product_repository.update_product,
-                              product)
+        self.assertRaises(InvalidPriceForProductException, self.product_repository.update_product,
+                          product)
 
     def test_product_updated_with_zero_price_raises_exception(self):
         product = ProductGenerator.generate_one_product()
+        product = insert_product_and_return_it(product)
+        product.price = CUPMoney('0.00')
 
-        with create_test_session() as session:
-            session.add(product)
-            session.commit()
-
-            product.price = CUPMoney('0.00')
-            self.assertRaises(InvalidPriceForProductException, self.product_repository.update_product,
-                              product)
+        self.assertRaises(InvalidPriceForProductException, self.product_repository.update_product,
+                          product)
 
     def test_product_updated_with_negative_profit_raises_exception(self):
         product = ProductGenerator.generate_one_product()
+        product = insert_product_and_return_it(product)
+        product.profit = CUPMoney('-1.00')
 
-        with create_test_session() as session:
-            session.add(product)
-            session.commit()
-
-            product.profit = CUPMoney('-1.00')
-            self.assertRaises(NegativeProfitForProductException, self.product_repository.update_product,
-                              product)
+        self.assertRaises(NegativeProfitForProductException, self.product_repository.update_product,
+                          product)
 
     def test_get_all_products_returns_empty_list(self):
         empty_list = []
@@ -205,10 +172,8 @@ class TestProductRepository(unittest.TestCase):
 
     def test_get_all_products_returns_nonempty_list(self):
         products = ProductGenerator.generate_products_by_quantity(3)
+        insert_products_in_database(products)
+        products = get_all_products_in_database()
 
-        with create_test_session() as session:
-            session.add_all(products)
-            session.commit()
-
-            retrieved_products = self.product_repository.get_all_products()
-            self.assertEqual(products, retrieved_products)
+        retrieved_products = self.product_repository.get_all_products()
+        self.assertEqual(products, retrieved_products)
--- tests/util/general.py
+++ tests/util/general.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
+
+from model.entity.models import Product
 
 TEST_DB_URL = 'sqlite:///test.db'
 
@@ -7,3 +9,72 @@ TEST_DB_URL = 'sqlite:///test.db'
 def create_test_session() -> Session:
     engine = create_engine(TEST_DB_URL)
     return Session(engine)
+
+
+def __get_unlinked_copy_of_product(product: Product) -> Product:
+    product_copy = Product()
+    product_copy.id = product.id
+    product_copy.name = product.name
+    product_copy.description = product.description
+    product_copy.price = product.price
+    product_copy.profit = product.profit
+    product_copy.quantity = product.quantity
+    return product_copy
+
+
+def insert_product_and_return_it(product: Product) -> Product:
+    """
+    Insert a product into the test database, and return it. The object
+    returned is not linked with a database session and errors will not
+    be thrown when accessing a field of the object.
+
+    :param Product product:
+    :return Product:
+    """
+    with create_test_session() as session:
+        session.add(product)
+        session.commit()
+        return __get_unlinked_copy_of_product(product)
+
+
+def insert_products_in_database(products: list):
+    with create_test_session() as session:
+        session.add_all(products)
+        session.commit()
+
+
+def get_one_product_from_database() -> Product:
+    """
+    Returns the first product from the database. The object is not
+    linked with a database session.
+
+    :return Product:
+    """
+    with create_test_session() as session:
+        product_linked_to_session = session.scalar(select(Product))
+        return __get_unlinked_copy_of_product(product_linked_to_session)
+
+
+def get_all_products_in_database() -> list:
+    """
+    Return all products in database. The returned products are not linked
+    with any database session.
+
+    :return list:
+    """
+
+    with create_test_session() as session:
+
+        products_linked_to_session = session.scalars(select(Product)).all()
+        products_to_return = map(__get_unlinked_copy_of_product, products_linked_to_session)
+
+        return list(products_to_return)
+
+
+def delete_all_products_from_database():
+
+    with create_test_session() as session:
+        statement = select(Product)
+        for a_product in session.scalars(statement):
+            session.delete(a_product)
+        session.commit()
