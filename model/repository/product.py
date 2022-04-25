d3fc3a23594fbf2b1b8e7510e2c30953105cd88f
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-25 18:51:11 -0400

    Feat: Cambios en el método de filtrar productos para cumplir con el test de filtrar productos por ganancias.

--- model/repository/product.py
+++ model/repository/product.py
@@ -181,4 +181,9 @@ class ProductRepository:
             query = query.where(Product.price >= the_filter.more_than_price)
         if the_filter.less_than_price is not None:
             query = query.where(Product.price <= the_filter.less_than_price)
+
+        if the_filter.more_than_profit is not None:
+            query = query.where(Product.profit >= the_filter.more_than_profit)
+        if the_filter.less_than_profit is not None:
+            query = query.where(Product.profit <= the_filter.less_than_profit)
         return query
