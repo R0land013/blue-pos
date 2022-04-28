d4c7a99070b24bfe723ea7fa6ecf078828304e64
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-28 12:39:59 -0400

    Refactor: Cambio en el generador de productos, para que los productos se generen con una cantidad de 5.
    	- Con este cambio todo producto generado tendrá 5 en cantidad, y no se
    	perderá menos tiempo y líneas de código, en los tests de ventas,
    	definiendo cuántos productos hay.

--- tests/util/generators/product.py
+++ tests/util/generators/product.py
@@ -25,7 +25,7 @@ class ProductGenerator:
         a_product = Product()
         a_product.name = fake.company()
         a_product.description = fake.text()
-        a_product.quantity = 0
+        a_product.quantity = 5
         a_product.price = ProductGenerator.DEFAULT_MONEY
         a_product.profit = ProductGenerator.DEFAULT_MONEY
 
