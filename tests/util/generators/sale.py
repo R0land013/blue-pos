0be17289de848092426419548d30ca05eeabf5fb
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-27 20:02:54 -0400

    Feat: Creado el generador de ventas, una utilidad para los tests.

---
+++ tests/util/generators/sale.py
@@ -0,0 +1,26 @@
+from datetime import date
+from model.entity.models import Sale, Product
+
+
+class SaleGenerator:
+
+    DEFAULT_DATE = date.today()
+
+    @staticmethod
+    def generate_sales_from_product(product: Product, quantity: int):
+        sales = []
+
+        for _ in range(quantity):
+            sale = SaleGenerator.generate_one_sale_from_product(product)
+            sales.append(sale)
+        return sales
+
+    @staticmethod
+    def generate_one_sale_from_product(product: Product) -> Sale:
+        sale = Sale()
+        sale.date = SaleGenerator.DEFAULT_DATE
+        sale.product = product
+        sale.price = product.price
+        sale.profit = product.profit
+
+        return sale
