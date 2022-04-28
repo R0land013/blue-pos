26e3994d80b41886e7664595ae23de27a4b36bb1
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-04-28 16:15:45 -0400

    Feat: Cambios en la configuración de los tests. Ahora siempre se intentará borrar la base de datos de pruebas antes de iniciar los tests.
    	- Esto se hace para que no quede la base de datos en un estado
    	inconsistente, y no reconocido por los tests. Esto podía pasar
    	si se interrumpía la ejecución de la suite de tests cuando todavía
    	no había terminado.

--- tests/settings.py
+++ tests/settings.py
@@ -3,12 +3,17 @@ from sqlalchemy import create_engine
 from pathlib import Path
 
 
+def __delete_test_database():
+    db_file = Path('test.db')
+    if db_file.exists():
+        db_file.unlink()
+
+
 def set_up():
+    __delete_test_database()
     engine = create_engine("sqlite:///test.db", future=True)
     Base.metadata.create_all(engine)
 
 
 def tear_down():
-    db_file = Path('test.db')
-    if db_file.exists():
-        db_file.unlink()
+    __delete_test_database()
