c1cbddd0acedb2cba14c8607da678919cdf9512d
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-05-05 17:38:14 -0400

    Feat: Cambios en el método de insertar ventas para que cuando se intente realizar una cantidad de ventas mayor a la cantidad de productos disponibles, se lance una excepción.
    	- Los tests se ejecutaron sin fallas.

--- model/repository/sale.py
+++ model/repository/sale.py
@@ -3,6 +3,8 @@ from sqlalchemy import insert, update, select
 from model.entity.models import Product, Sale
 from sqlalchemy.orm import Session
 
+from model.repository.exc.sale import NoEnoughProductsException
+
 
 class SaleRepository:
 
@@ -11,6 +13,8 @@ class SaleRepository:
 
     def insert_sales(self, sale: Sale, quantity: int):
         self.__check_quantity_is_positive(quantity)
+        product = self.__get_product_by_id(sale.product_id)
+        self.__check_there_are_enough_products(product, quantity)
 
         self.__execute_insertion(sale, quantity)
         self.__execute_product_quantity_update(sale, quantity)
@@ -19,6 +23,13 @@ class SaleRepository:
     def __check_quantity_is_positive(self, quantity: int):
         if quantity <= 0:
             raise ValueError('The quantity of sales must be positive.')
+
+    def __get_product_by_id(self, product_id: int) -> Product:
+        return self.__session.scalar(select(Product).where(Product.id == product_id))
+
+    def __check_there_are_enough_products(self, product: Product, sale_quantity: int):
+        if product.quantity - sale_quantity < 0:
+            raise NoEnoughProductsException(product.quantity)
 
     def __execute_insertion(self, sale: Sale, quantity: int):
         for i in range(quantity):
