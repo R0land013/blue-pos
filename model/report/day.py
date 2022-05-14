e4f307dc15b6368f92d9a5eb1f124d649c0e7def
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-05-14 18:09:56 -0400

    Test: Creada prueba que verifica el correcto funcionamiento del método de obtener las ventas de un reporte diario.

--- model/report/day.py
+++ model/report/day.py
@@ -1,8 +1,9 @@
 from datetime import date
 from model.report.abstract_report import AbstractSaleReport
+from model.repository.sale import SaleRepository
 
 
 class DaySaleReport(AbstractSaleReport):
 
-    def __init__(self, day_date: date):
+    def __init__(self, day_date: date, repository: SaleRepository):
         raise NotImplementedError()
--- tests/model/report/test_day_sale_report.py
+++ tests/model/report/test_day_sale_report.py
@@ -1,14 +1,36 @@
 import unittest
+from datetime import date, timedelta
 
+from model.report.day import DaySaleReport
 from model.repository.factory import RepositoryFactory
-from tests.util.general import TEST_DB_URL, delete_all_products_from_database
+from tests.util.general import TEST_DB_URL, delete_all_products_from_database, insert_product_and_return_it, \
+    insert_sales_and_return_them, assert_sale_lists_are_equal_ignoring_id
+from tests.util.generators.product import ProductGenerator
+from tests.util.generators.sale import SaleGenerator
 
 
 class TestDaySaleReport(unittest.TestCase):
 
     def setUp(self) -> None:
         self.sale_repository = RepositoryFactory.get_sale_repository(TEST_DB_URL)
+        self.TODAY = date.today()
+        self.YESTERDAY = self.TODAY - timedelta(days=1)
 
     def tearDown(self):
         RepositoryFactory.close_session()
         delete_all_products_from_database()
+
+    def test_get_sales(self):
+        product = ProductGenerator.generate_one_product()
+        product = insert_product_and_return_it(product)
+        sales = SaleGenerator.generate_sales_from_product(product, 3)
+        s1, s2, s3 = sales
+        s1.date = self.YESTERDAY
+        s2.date = self.TODAY
+        s3.date = self.YESTERDAY
+        sales = insert_sales_and_return_them(sales)
+
+        report = DaySaleReport(self.YESTERDAY, self.sale_repository)
+        sales_of_report = report.get_sales()
+
+        assert_sale_lists_are_equal_ignoring_id(sales_of_report, [s1, s2])
