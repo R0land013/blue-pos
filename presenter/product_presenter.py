dd158296dee7e0d2c58e083a3a0e070d4990fcc6
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-06-18 18:45:12 -0400

    Feat: Implementada funcionalidad de editar productos.
    	Los campos de la GUI todavía no tienen validación.
    
    	Se encontró un bug en el repositorio de productos, en el método
    	de actualizar.

--- presenter/product_presenter.py
+++ presenter/product_presenter.py
@@ -18,6 +18,15 @@ class ProductPresenter(AbstractPresenter):
     def _on_initialize(self):
         self.__initialize_view()
         self.__product_repo = RepositoryFactory.get_product_repository()
+        self.__product_to_edit = self.__get_product_if_action_is_to_edit()
+
+    def __get_product_if_action_is_to_edit(self) -> Product:
+        if self._get_intent_action() == self.EDIT_PRODUCT_ACTION:
+            a_filter = ProductFilter()
+            a_filter.id = self._get_intent_data()[self.PRODUCT_ID]
+            product = self.__product_repo.get_products_by_filter(a_filter)[0]
+            return product
+        return None
 
     def __initialize_view(self):
         view = ProductView(self)
@@ -34,10 +43,7 @@ class ProductPresenter(AbstractPresenter):
 
     def __fill_product_form_if_action_is_for_editing_product(self):
         if self._get_intent_action() == self.EDIT_PRODUCT_ACTION:
-            a_filter = ProductFilter()
-            a_filter.id = self._get_intent_data()[self.PRODUCT_ID]
-            product = self.__product_repo.get_products_by_filter(a_filter)[0]
-            self.__fill_product_form(product)
+            self.__fill_product_form(self.__product_to_edit)
 
     def __fill_product_form(self, product: Product):
         view = self.get_view()
@@ -49,6 +55,18 @@ class ProductPresenter(AbstractPresenter):
         view.set_quantity(product.quantity)
 
     def save_product(self):
+        if self._get_intent_action() == self.NEW_PRODUCT_ACTION:
+            self.__insert_new_product()
+        elif self._get_intent_action() == self.EDIT_PRODUCT_ACTION:
+            self.__update_product()
+
+    def __insert_new_product(self):
+
+        product = self.__construct_product_instance_from_view_fields()
+        self.__product_repo.insert_product(product)
+        self._close_this_presenter()
+
+    def __construct_product_instance_from_view_fields(self):
         view = self.get_view()
         name = view.get_name()
         description = view.get_description()
@@ -56,7 +74,7 @@ class ProductPresenter(AbstractPresenter):
         profit = CUPMoney(view.get_profit())
         quantity = view.get_quantity()
 
-        product = Product(
+        return Product(
             name=name,
             description=description,
             price=price,
@@ -64,7 +82,11 @@ class ProductPresenter(AbstractPresenter):
             quantity=quantity
         )
 
-        self.__product_repo.insert_product(product)
+    def __update_product(self):
+        product = self.__construct_product_instance_from_view_fields()
+        product.id = self._get_intent_data()[self.PRODUCT_ID]
+        
+        self.__product_repo.update_product(product)
         self._close_this_presenter()
 
     def go_back(self):
