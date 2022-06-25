f6372ce29a68856cdb2b042f367e15ac17ab04cf
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-06-25 18:06:01 -0400

    Feat: Implementado el método get_sales de MonthSaleReport. Los tests pasaron.
    	Fix: Los tests a MonthSaleReport no borraban los productos y las ventas
    	cuando se terminaban de ejecutar.
    
    	Test: Se hicieron algunos cambios para mejorar el test que comprueba el
    	funcionamiento de get_sales de MonthSaleReport.

--- model/report/month.py
+++ model/report/month.py
@@ -1,15 +1,31 @@
-from datetime import date
+from datetime import date, timedelta
 from model.report.abstract_report import AbstractSaleReport
-from model.repository.sale import SaleRepository
+from model.repository.sale import SaleRepository, SaleFilter
 
 
 class MonthSaleReport(AbstractSaleReport):
 
     def __init__(self, month_date: date, sale_repository: SaleRepository):
-        pass
+        self.__month_date = month_date
+        self.__sale_repo = sale_repository
 
     def get_report_as_html(self) -> str:
         pass
 
     def get_sales(self) -> list:
-        pass
+        first_date_of_month = date(day=1,
+                                   month=self.__month_date.month,
+                                   year=self.__month_date.year)
+        last_date_of_month = self.__get_last_date_of_month()
+        a_filter = SaleFilter()
+        a_filter.minimum_date = first_date_of_month
+        a_filter.maximum_date = last_date_of_month
+        return self.__sale_repo.get_sales_by_filter(a_filter)
+
+    def __get_last_date_of_month(self):
+        if self.__month_date.month == 12:
+            month = 1
+        else:
+            month = self.__month_date.month + 1
+        first_date_next_month = date(day=1, month=month, year=self.__month_date.year)
+        return first_date_next_month - timedelta(1)
--- tests/model/report/test_month_sale_report.py
+++ tests/model/report/test_month_sale_report.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 from model.report.month import MonthSaleReport
 from model.repository.factory import RepositoryFactory
 from tests.util.general import TEST_REPORT_PATH, TEST_DB_URL, insert_product_and_return_it, \
-    insert_sales_and_return_them, assert_sale_lists_are_equal_ignoring_id
+    insert_sales_and_return_them, assert_sale_lists_are_equal_ignoring_id, delete_all_products_from_database
 from tests.util.generators.product import ProductGenerator
 from tests.util.generators.sale import SaleGenerator
 
@@ -15,24 +15,39 @@ class TestMonthSaleReport(TestCase):
         self.sale_repository = RepositoryFactory.get_sale_repository(TEST_DB_URL)
         this_month = date.today().month
         this_year = date.today().year
-        self.THIS_MONTH_DATE = date(this_year, this_month, 1)
-        self.PREVIOUS_MONTH_DATE = self.THIS_MONTH_DATE - timedelta(days=1)
+        self.FIRST_DATE_OF_THIS_MONTH = date(this_year, this_month, 1)
+        self.LAST_DATE_OF_THIS_MONTH = self.__get_last_date_of_this_month()
+        self.LAST_DATE_OF_PREVIOUS_MONTH = self.FIRST_DATE_OF_THIS_MONTH - timedelta(days=1)
         self.HTML_MONTH_REPORT_PATH = TEST_REPORT_PATH.joinpath('month_report.html')
         self.PDF_MONTH_REPORT_PATH = TEST_REPORT_PATH.joinpath('month_report.pdf')
+
+    def __get_last_date_of_this_month(self):
+        if self.FIRST_DATE_OF_THIS_MONTH.month == 12:
+            next_month = 1
+        else:
+            next_month = self.FIRST_DATE_OF_THIS_MONTH.month + 1
+        this_year = self.FIRST_DATE_OF_THIS_MONTH.year
+        first_date_next_month = date(day=1, month=next_month, year=this_year)
+        return first_date_next_month - timedelta(days=1)
+
+    def tearDown(self):
+        RepositoryFactory.close_session()
+        delete_all_products_from_database()
 
     def test_get_sales(self):
         product = ProductGenerator.generate_one_product()
         product = insert_product_and_return_it(product)
         sales = SaleGenerator.generate_sales_from_product(product, 5)
         s1, s2, s3, s4, s5 = sales
-        s1.date = self.PREVIOUS_MONTH_DATE - timedelta(days=1)
-        s2.date = self.PREVIOUS_MONTH_DATE - timedelta(days=2)
-        s3.date = self.THIS_MONTH_DATE + timedelta(days=1)
-        s4.date = self.THIS_MONTH_DATE + timedelta(days=2)
-        s5.date = self.THIS_MONTH_DATE + timedelta(days=3)
+        s1.date = self.LAST_DATE_OF_PREVIOUS_MONTH - timedelta(days=1)
+        s2.date = self.LAST_DATE_OF_PREVIOUS_MONTH - timedelta(days=2)
+
+        s3.date = self.FIRST_DATE_OF_THIS_MONTH
+        s4.date = self.FIRST_DATE_OF_THIS_MONTH + timedelta(days=10)
+        s5.date = self.LAST_DATE_OF_THIS_MONTH
         insert_sales_and_return_them(sales)
 
-        report = MonthSaleReport(self.THIS_MONTH_DATE, self.sale_repository)
+        report = MonthSaleReport(self.FIRST_DATE_OF_THIS_MONTH, self.sale_repository)
         sales_of_report = report.get_sales()
 
-        assert_sale_lists_are_equal_ignoring_id(sales_of_report, [s3, s4, s5])
+        self.assertEqual(sales_of_report, [s3, s4, s5])
