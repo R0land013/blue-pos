38ce188a0c211822e3def9b7c86f24899dd69159
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-06-27 10:27:48 -0400

    Feat: Implementado el método get_report_as_html de YearSaleReport, y creada la plantilla year_report.html.

--- model/report/year.py
+++ model/report/year.py
@@ -1,5 +1,9 @@
 from datetime import date
 
+from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2.nodes import Template
+
+from model.economy import calculate_total_profit, calculate_collected_money
 from model.report.abstract_report import AbstractSaleReport
 from model.repository.sale import SaleRepository, SaleFilter
 
@@ -21,4 +25,19 @@ class YearSaleReport(AbstractSaleReport):
         return self.__sale_repo.get_sales_by_filter(sale_filter)
 
     def get_report_as_html(self) -> str:
-        pass
+        sales = self.get_sales()
+        total_profit = calculate_total_profit(sales)
+        total_collected_money = calculate_collected_money(sales)
+
+        template = self.get_template()
+        return template.render(date=self.__year_date,
+                               sales=sales,
+                               total_profit=total_profit,
+                               total_collected_money=total_collected_money)
+
+    def get_template(self) -> Template:
+        env = Environment(
+            loader=PackageLoader('model.report'),
+            autoescape=select_autoescape()
+        )
+        return env.get_template('year_report.html')
