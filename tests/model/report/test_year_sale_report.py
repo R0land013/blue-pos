fd6326401ae80a32912a9c70a90074563e4f1e7e
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-06-27 10:31:11 -0400

    Test: Creada prueba para verificar la generación del reporte anual como pdf.

--- tests/model/report/test_year_sale_report.py
+++ tests/model/report/test_year_sale_report.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 from unittest import TestCase
 
-from model.report.generators import generate_html_file
+from model.report.generators import generate_html_file, generate_pdf_file
 from model.report.year import YearSaleReport
 from model.repository.factory import RepositoryFactory
 from tests.util.general import TEST_DB_URL, TEST_REPORT_PATH, delete_all_products_from_database, \
@@ -56,3 +56,18 @@ class TestYearSaleReport(TestCase):
 
         report = YearSaleReport(self.FIRST_DATE_OF_THIS_YEAR, self.sale_repository)
         generate_html_file(self.HTML_YEAR_REPORT_PATH, report)
+
+    def test_pdf_report_is_generated(self):
+        product = ProductGenerator.generate_one_product()
+        product = insert_product_and_return_it(product)
+        sales = SaleGenerator.generate_sales_from_product(product, 5)
+        s1, s2, s3, s4, s5 = sales
+        s1.date = self.LAST_DATE_OF_PREVIOUS_YEAR
+        s2.date = self.FIRST_DATE_OF_THIS_YEAR
+        s3.date = self.FIRST_DATE_OF_THIS_YEAR + timedelta(days=60)
+        s4.date = self.LAST_DATE_OF_THIS_YEAR
+        s5.date = self.FIRST_DATE_OF_NEXT_YEAR
+        insert_sales_and_return_them(sales)
+
+        report = YearSaleReport(self.FIRST_DATE_OF_THIS_YEAR, self.sale_repository)
+        generate_pdf_file(self.PDF_YEAR_REPORT_PATH, report)
