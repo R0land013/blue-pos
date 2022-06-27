7dba33f2bca69c4cc789bb746ac161c12bed549d
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-06-27 10:18:22 -0400

    Test: Creado test para generar el reporte anual de ventas como fichero html.
    	Fix: Pequeño arreglo en las variables de la clase de TestYearSaleReport.

--- tests/model/report/test_year_sale_report.py
+++ tests/model/report/test_year_sale_report.py
@@ -1,6 +1,7 @@
 from datetime import date, timedelta
 from unittest import TestCase
 
+from model.report.generators import generate_html_file
 from model.report.year import YearSaleReport
 from model.repository.factory import RepositoryFactory
 from tests.util.general import TEST_DB_URL, TEST_REPORT_PATH, delete_all_products_from_database, \
@@ -17,8 +18,8 @@ class TestYearSaleReport(TestCase):
         self.LAST_DATE_OF_THIS_YEAR = date(day=31, month=12, year=date.today().year)
         self.LAST_DATE_OF_PREVIOUS_YEAR = date(day=31, month=12, year=date.today().year - 1)
         self.FIRST_DATE_OF_NEXT_YEAR = date(day=1, month=1, year=date.today().year + 1)
-        self.HTML_MONTH_REPORT_PATH = TEST_REPORT_PATH.joinpath('year_report.html')
-        self.PDF_MONTH_REPORT_PATH = TEST_REPORT_PATH.joinpath('year_report.pdf')
+        self.HTML_YEAR_REPORT_PATH = TEST_REPORT_PATH.joinpath('year_report.html')
+        self.PDF_YEAR_REPORT_PATH = TEST_REPORT_PATH.joinpath('year_report.pdf')
 
     def tearDown(self):
         RepositoryFactory.close_session()
@@ -40,3 +41,18 @@ class TestYearSaleReport(TestCase):
         report_sales = report.get_sales()
 
         self.assertEqual(report_sales, [s2, s3, s4])
+
+    def test_html_report_is_generated(self):
+        product = ProductGenerator.generate_one_product()
+        product = insert_product_and_return_it(product)
+        sales = SaleGenerator.generate_sales_from_product(product, 5)
+        s1, s2, s3, s4, s5 = sales
+        s1.date = self.LAST_DATE_OF_PREVIOUS_YEAR
+        s2.date = self.FIRST_DATE_OF_THIS_YEAR
+        s3.date = self.FIRST_DATE_OF_THIS_YEAR + timedelta(days=60)
+        s4.date = self.LAST_DATE_OF_THIS_YEAR
+        s5.date = self.FIRST_DATE_OF_NEXT_YEAR
+        insert_sales_and_return_them(sales)
+
+        report = YearSaleReport(self.FIRST_DATE_OF_THIS_YEAR, self.sale_repository)
+        generate_html_file(self.HTML_YEAR_REPORT_PATH, report)
