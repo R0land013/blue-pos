cd35e3538d0e2fee7caa1cd4de9e41e277774b29
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-06-28 11:58:33 -0400

    Feat: La tabla de las ventas de un producto recibió su formato.

--- view/product_sale_management.py
+++ view/product_sale_management.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QFrame
+from PyQt5.QtWidgets import QFrame, QTableWidget
 from PyQt5.uic import loadUi
 
 
@@ -8,8 +8,24 @@ class ProductSaleManagementView(QFrame):
         super().__init__()
         self.__presenter = presenter
 
+        self.set_up_gui()
+
+    def set_up_gui(self):
         loadUi('./view/ui/product_sale_management.ui', self)
+        self.__set_table_format()
         self.__wire_up_connections()
 
     def __wire_up_connections(self):
         self.back_button.clicked.connect(self.__presenter.close_presenter)
+
+    def __set_table_format(self):
+        self.sale_table.setColumnCount(4)
+        self.sale_table.setHorizontalHeaderLabels([
+            'Id. Venta',
+            'Pago',
+            'Ganancia',
+            'Fecha'
+        ])
+        self.sale_table.resizeColumnsToContents()
+        self.sale_table.setSelectionBehavior(QTableWidget.SelectionBehavior.SelectRows)
+        self.sale_table.setSelectionMode(QTableWidget.SelectionMode.ExtendedSelection)
