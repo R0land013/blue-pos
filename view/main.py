f0bc942aa7cee651e7b100795d3b4a666d2ad7a5
Rolando Rio Garaboa <rolandorio06@gmail.com>
2022-07-02 08:53:38 -0400

    Style: La ventana principal aparecerá con un tamaño igual a la mitad de la pantalla.
    	Este tamaño será el mínimo de la ventana.

--- view/main.py
+++ view/main.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QFrame
+from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QFrame, QApplication
 from PyQt5.uic import loadUi
 
 
@@ -8,8 +9,18 @@ class MainView(QFrame):
         super().__init__()
         self.__presenter = presenter
 
+        self.__set_up_gui()
+
+    def __set_up_gui(self):
         loadUi('./view/ui/main.ui', self)
         self.wire_up_gui_connections()
+        self.__set_window_minimum_size_to_half_of_screen()
 
     def wire_up_gui_connections(self):
         self.product_management_button.clicked.connect(self.__presenter.open_product_management)
+
+    def __set_window_minimum_size_to_half_of_screen(self):
+        q_screen_size = QApplication.primaryScreen().size()
+        new_width = q_screen_size.width() // 2
+        new_height = q_screen_size.height() // 2
+        self.window().setMinimumSize(QSize(new_width, new_height))
