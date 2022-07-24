a7f734c5588826edfee932412ad6196adb1bf06b
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-07-24 13:43:32 -0400

    Feat: Implementado el método get_report_statistics en la clase DaySaleReport.

--- model/report/day.py
+++ model/report/day.py
@@ -1,6 +1,7 @@
 from datetime import date
 from model.economy import calculate_total_profit, calculate_collected_money
 from model.report.abstract_report import AbstractSaleReport
+from model.report.statistics import ReportStatistic
 from model.repository.sale import SaleRepository, SaleFilter
 from jinja2 import Environment, PackageLoader, select_autoescape, Template
 
@@ -10,6 +11,7 @@ class DaySaleReport(AbstractSaleReport):
     def __init__(self, day_date: date, repository: SaleRepository):
         self.__day_date = day_date
         self.__sale_repository = repository
+        self.__report_statistic = None
 
     def get_sales(self) -> list:
         sale_filter = SaleFilter()
@@ -36,3 +38,11 @@ class DaySaleReport(AbstractSaleReport):
         )
         return env.get_template('day_report.html')
 
+    def get_report_statistics(self) -> ReportStatistic:
+        sales = self.get_sales()
+        profit_money = calculate_total_profit(sales)
+        collected_money = calculate_collected_money(sales)
+
+        return ReportStatistic(sale_quantity=len(sales), paid_money=collected_money,
+                               profit_money=profit_money, initial_date=self.__day_date,
+                               final_date=self.__day_date)
--- model/report/statistics.py
+++ model/report/statistics.py
@@ -6,19 +6,23 @@ class ReportStatistic:
 
     def __init__(self, sale_quantity: int, paid_money: Money, profit_money: Money,
                  initial_date: date, final_date: date):
-        pass
+        self.__sale_quantity = sale_quantity
+        self.__paid_money = paid_money
+        self.__profit_money = profit_money
+        self.__initial_date = initial_date
+        self.__final_date = final_date
 
     def sale_quantity(self) -> int:
-        pass
+        return self.__sale_quantity
 
     def paid_money(self) -> Money:
-        pass
+        return self.__paid_money
 
     def profit_money(self) -> Money:
-        pass
+        return self.__profit_money
 
     def initial_date(self) -> date:
-        pass
+        return self.__initial_date
 
     def final_date(self) -> date:
-        pass
+        return self.__final_date
