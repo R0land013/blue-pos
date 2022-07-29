197e7dbd90233e520a9c85095a628ae5fb67009c
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-07-29 08:57:49 -0400

    Feat: Las elementos son cargados en la tabla de la vista de reporte mensual.

--- presenter/month_report.py
+++ presenter/month_report.py
@@ -1,5 +1,9 @@
 from easy_mvp.abstract_presenter import AbstractPresenter
 
+from model.entity.models import Sale
+from model.report.month import MonthSaleReport
+from model.repository.factory import RepositoryFactory
+from presenter.util.thread_worker import PresenterThreadWorker
 from view.month_report import MonthSaleReportView
 
 
@@ -7,9 +11,48 @@ class MonthSaleReportPresenter(AbstractPresenter):
 
     def _on_initialize(self):
         self.__initialize_view()
+        self.__sale_repo = RepositoryFactory.get_sale_repository()
 
     def __initialize_view(self):
         self._set_view(MonthSaleReportView(self))
 
     def close_presenter(self):
         self._close_this_presenter()
+
+    def execute_thread_to_generate_report_on_gui(self):
+        self.thread = PresenterThreadWorker(self.__load_report_sales)
+        self.thread.when_started.connect(self.__disable_gui_and_show_processing_message)
+        self.thread.when_finished.connect(self.__fill_table)
+        self.thread.when_finished.connect(self.__set_available_gui_and_show_no_state_bar_message)
+        self.thread.start()
+
+    def __load_report_sales(self, thread: PresenterThreadWorker):
+        self.__month_report = MonthSaleReport(month_date=self.get_view().get_date(),
+                                            sale_repository=self.__sale_repo)
+        self.__sales = self.__month_report.get_sales()
+
+    def __disable_gui_and_show_processing_message(self):
+        self.get_view().set_disabled_view_except_status_bar(True)
+        self.get_view().set_state_bar_message('Creando reporte...')
+
+    def __fill_table(self):
+        self.get_view().clean_table()
+        for a_sale in self.__sales:
+            self.__insert_sale_on_table(a_sale)
+        self.get_view().resize_table_columns_to_contents()
+
+    def __insert_sale_on_table(self, sale: Sale):
+        view = self.get_view()
+        view.add_empty_row_at_the_end_of_table()
+        row = view.get_last_table_row_index()
+
+        view.set_cell_on_table(row, MonthSaleReportView.SALE_ID_COLUMN, str(sale.id))
+        view.set_cell_on_table(row, MonthSaleReportView.PRODUCT_NAME_COLUMN, str(sale.product.name))
+        view.set_cell_on_table(row, MonthSaleReportView.PRODUCT_ID_COLUMN, str(sale.product.id))
+        view.set_cell_on_table(row, MonthSaleReportView.SALE_PRICE_COLUMN, str(sale.price))
+        view.set_cell_on_table(row, MonthSaleReportView.SALE_PROFIT_COLUMN, str(sale.profit))
+        view.set_cell_on_table(row, MonthSaleReportView.SALE_DATE_COLUMN, str(sale.date))
+
+    def __set_available_gui_and_show_no_state_bar_message(self):
+        self.get_view().set_disabled_view_except_status_bar(False)
+        self.get_view().set_state_bar_message('')
--- view/month_report.py
+++ view/month_report.py
@@ -1,8 +1,20 @@
-from PyQt5.QtWidgets import QFrame
+from datetime import date
+
+from PyQt5.QtCore import Qt, QDate
+from PyQt5.QtWidgets import QFrame, QTableWidget, QTableWidgetItem
 from PyQt5.uic import loadUi
+
+from view.util.table_columns import QCUPMoneyTableItem, QIntegerTableItem
 
 
 class MonthSaleReportView(QFrame):
+
+    SALE_ID_COLUMN = 0
+    PRODUCT_NAME_COLUMN = 1
+    PRODUCT_ID_COLUMN = 2
+    SALE_PRICE_COLUMN = 3
+    SALE_PROFIT_COLUMN = 4
+    SALE_DATE_COLUMN = 5
 
     def __init__(self, presenter):
         super().__init__()
@@ -13,6 +25,71 @@ class MonthSaleReportView(QFrame):
     def __setup_gui(self):
         loadUi('./view/ui/month_sale_report.ui', self)
         self.__wire_up_gui_connections()
+        self.__setup_date_edit()
+        self.__setup_table()
+
+    def __setup_table(self):
+        self.sale_report_table.setColumnCount(6)
+        self.sale_report_table.setHorizontalHeaderLabels([
+            'Id. Venta',
+            'Producto',
+            'Id. Producto',
+            'Pagado',
+            'Ganancia',
+            'Fecha'
+        ])
+        self.sale_report_table.resizeColumnsToContents()
+        self.sale_report_table.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
+
+    def __setup_date_edit(self):
+        self.month_date_edit.setDate(QDate.currentDate())
+        self.month_date_edit.setMaximumDate(QDate.currentDate())
 
     def __wire_up_gui_connections(self):
         self.back_button.clicked.connect(self.__presenter.close_presenter)
+        self.create_report_button.clicked.connect(self.__presenter.execute_thread_to_generate_report_on_gui)
+
+    def get_date(self):
+        q_date = self.month_date_edit.date()
+        return date(day=q_date.day(),
+                    month=q_date.month(),
+                    year=q_date.year())
+
+    def set_sale_quantity(self, quantity: int):
+        self.sale_quantity_label.setText(str(quantity))
+
+    def set_paid_money(self, paid: str):
+        self.payment_money_label.setText(paid)
+
+    def set_profit_money(self, profit: str):
+        self.profit_money_label.setText(profit)
+
+    def set_disabled_view_except_status_bar(self, disable: bool):
+        self.main_content_frame.setDisabled(disable)
+        self.export_as_button.setDisabled(disable)
+
+    def set_state_bar_message(self, message: str):
+        self.state_bar_label.setText(message)
+
+    def clean_table(self):
+        while self.sale_report_table.rowCount() > 0:
+            self.sale_report_table.removeRow(0)
+
+    def add_empty_row_at_the_end_of_table(self):
+        new_row_index = self.sale_report_table.rowCount()
+        self.sale_report_table.insertRow(new_row_index)
+
+    def set_cell_on_table(self, row: int, column: int, data):
+
+        item = QTableWidgetItem(str(data))
+        if column == self.SALE_PRICE_COLUMN or column == self.SALE_PROFIT_COLUMN:
+            item = QCUPMoneyTableItem(str(data))
+        elif column == self.SALE_ID_COLUMN or column == self.PRODUCT_ID_COLUMN:
+            item = QIntegerTableItem(str(data))
+        self.sale_report_table.setItem(row, column, item)
+
+    def get_last_table_row_index(self) -> int:
+        return self.sale_report_table.rowCount() - 1
+
+    def resize_table_columns_to_contents(self):
+        self.sale_report_table.resizeColumnsToContents()
