caa20279146ef5a575dc0449caafeb1d4deede7e
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-07-30 11:46:32 -0400

    Test: Para verificar la generación del reporte semanal de ventas como un fichero html

--- tests/model/report/test_week_sale_report.py
+++ tests/model/report/test_week_sale_report.py
@@ -1,10 +1,11 @@
 import unittest
 from datetime import date, timedelta
 
+from model.report.generators import generate_html_file
 from model.report.week import WeekSaleReport
 from model.repository.factory import RepositoryFactory
 from tests.util.general import TEST_DB_URL, delete_all_products_from_database, \
-    insert_products_in_database_and_return_them, insert_sales_and_return_them
+    insert_products_in_database_and_return_them, insert_sales_and_return_them, TEST_REPORT_PATH
 from tests.util.generators.product import ProductGenerator
 from tests.util.generators.sale import SaleGenerator
 
@@ -19,6 +20,8 @@ class TestWeekSaleReport(unittest.TestCase):
         self.SUNDAY_DATE_THIS_WEEK = self.__get_sunday_date_of_this_week()
         self.WEDNESDAY_DATE_THIS_WEEK = self.__get_wednesday_date_of_this_week()
         self.MONDAY_DATE_NEXT_WEEK = self.__get_monday_date_next_week()
+
+        self.HTML_WEEK_REPORT_PATH = TEST_REPORT_PATH.joinpath('week_report.html')
 
     @staticmethod
     def __get_monday_date_of_this_week() -> date:
@@ -72,3 +75,22 @@ class TestWeekSaleReport(unittest.TestCase):
         week_report_sales = week_report.get_sales()
 
         self.assertEqual(week_report_sales, [s2, s3, s4, s5])
+
+    def test_html_report_is_correctly_generated(self):
+        products = ProductGenerator.generate_products_by_quantity(2)
+        p1, p2 = insert_products_in_database_and_return_them(products)
+        sales_of_p1 = SaleGenerator.generate_sales_from_product(p1, 3)
+        sales_of_p2 = SaleGenerator.generate_sales_from_product(p2, 3)
+        s1, s2, s3 = sales_of_p1
+        s4, s5, s6 = sales_of_p2
+        s1.date = self.SUNDAY_DATE_PREVIOUS_WEEK
+        s2.date = self.MONDAY_DATE_THIS_WEEK
+        s3.date = self.WEDNESDAY_DATE_THIS_WEEK
+        s4.date = self.WEDNESDAY_DATE_THIS_WEEK
+        s5.date = self.SUNDAY_DATE_THIS_WEEK
+        s6.date = self.MONDAY_DATE_NEXT_WEEK
+        s1, s2, s3 = insert_sales_and_return_them(sales_of_p1)
+        s4, s5, s6 = insert_sales_and_return_them(sales_of_p2)
+
+        week_report = WeekSaleReport(self.WEDNESDAY_DATE_THIS_WEEK, self.sale_repository)
+        generate_html_file(self.HTML_WEEK_REPORT_PATH, week_report)
