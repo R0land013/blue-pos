2ad803e9a1c553fb089d44f71f51c5f30649f91f
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-07-30 11:42:05 -0400

    Feat: Implementado el método get_sales de WeekSaleReport
    	El test de este método se ejecutó sin fallas.

--- model/report/week.py
+++ model/report/week.py
@@ -1,17 +1,31 @@
-from datetime import date
+from datetime import date, timedelta
 
 from model.report.abstract_report import AbstractSaleReport
 from model.report.statistics import ReportStatistic
-from model.repository.sale import SaleRepository
+from model.repository.sale import SaleRepository, SaleFilter
 
 
 class WeekSaleReport(AbstractSaleReport):
 
     def __init__(self, week_day: date, sale_repository: SaleRepository):
-        pass
+        self.__week_day_date = week_day
+        self.__sale_repository = sale_repository
 
     def get_sales(self) -> list:
-        pass
+        week_filter = SaleFilter()
+        week_filter.minimum_date = self.__get_monday_date()
+        week_filter.maximum_date = self.get_sunday_date()
+
+        return self.__sale_repository.get_sales_by_filter(week_filter)
+
+    def __get_monday_date(self):
+        if self.__week_day_date.weekday() > 0:
+            return self.__week_day_date - timedelta(days=self.__week_day_date.weekday())
+        return self.__week_day_date
+
+    def get_sunday_date(self):
+        if self.__week_day_date.weekday() < 6:
+            return self.__week_day_date + timedelta(days=6 - self.__week_day_date.weekday())
 
     def get_report_as_html(self) -> str:
         pass
