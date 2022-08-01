41c91fdf7ff3677e84ac97d678670225ae28de6b
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-08-01 11:23:44 -0400

    Feat: Creada la clase QWeekCalendarSelectorWidget
    	Este widget facilita la selección de semanas de una calendario.
    	Con el método get_selected_date_range se obtiene la fecha del
    	primero y el último día de la semana. El rango que se obtiene
    	siempre se encontrará dentro de los límites especificados con
    	el método setDateRange.

---
+++ view/util/week_selector.py
@@ -0,0 +1,81 @@
+from datetime import date, timedelta
+from PyQt5.QtGui import QPalette, QTextCharFormat
+from PyQt5.QtWidgets import QCalendarWidget
+from PyQt5.QtCore import QDate
+
+
+class QWeekCalendarSelectorWidget(QCalendarWidget):
+    def __init__(self):
+        super().__init__()
+        self.__first_date_of_week = None
+        self.__last_date_of_week = None
+
+        self.__HIGHLIGHTED_CALENDAR_FORMAT = QTextCharFormat()
+        self.__HIGHLIGHTED_CALENDAR_FORMAT.setBackground(self.palette().brush(QPalette.Highlight))
+        self.__HIGHLIGHTED_CALENDAR_FORMAT.setForeground(self.palette().color(QPalette.HighlightedText))
+        self.__RESET_CALENDAR_FORMAT = QTextCharFormat()
+
+        self.clicked.connect(self.__change_selected_week)
+        self.selectionChanged.connect(self.__change_selected_week)
+        self.__change_selected_week(self.selectedDate())
+
+    def setDateRange(self, minimum_date: QDate, maximum_date: QDate):
+        super().setDateRange(minimum_date, maximum_date)
+        self.__change_selected_week(self.selectedDate())
+
+    def get_selected_date_range(self) -> tuple:
+        """Returns a tuple containing two elements. The first is the first
+        date of the week, and the second one the last date of the week."""
+        return self.__first_date_of_week, self.__last_date_of_week
+
+    def __set_calendar_format(self, a_format: QTextCharFormat):
+        if self.__first_date_of_week and self.__last_date_of_week:
+            initial_date = self.__first_date_of_week
+            final_date = self.__last_date_of_week
+
+            while initial_date <= final_date:
+                self.setDateTextFormat(initial_date, a_format)
+                initial_date = initial_date.addDays(1)
+
+    def __change_selected_week(self, qdate: QDate = None):
+        # Si este método es llamado por la señal selectionChanged
+        if qdate is None:
+            qdate = self.selectedDate()
+
+        self.__set_calendar_format(self.__RESET_CALENDAR_FORMAT)
+
+        self.__first_date_of_week = self.__get_first_qdate_of_week(qdate)
+        self.__last_date_of_week = self.__get_last_qdate_of_week(qdate)
+
+        self.__set_calendar_format(self.__HIGHLIGHTED_CALENDAR_FORMAT)
+
+    def __get_first_qdate_of_week(self, qdate: QDate) -> QDate:
+        py_date = self.__to_python_date(qdate)
+
+        if py_date.weekday() > 0:
+            first_week_py_date = py_date - timedelta(days=py_date.weekday())
+
+            if self.__to_qdate(first_week_py_date) < self.minimumDate():
+                return self.minimumDate()
+            return self.__to_qdate(first_week_py_date)
+
+        return qdate
+
+    def __get_last_qdate_of_week(self, qdate: QDate) -> QDate:
+        py_date = self.__to_python_date(qdate)
+
+        if py_date.weekday() < 6:
+            last_week_py_date = py_date + timedelta(days=6 - py_date.weekday())
+
+            if self.__to_qdate(last_week_py_date) > self.maximumDate():
+                return self.maximumDate()
+            return self.__to_qdate(last_week_py_date)
+        return qdate
+
+    @staticmethod
+    def __to_python_date(qdate: QDate) -> date:
+        return date(day=qdate.day(), month=qdate.month(), year=qdate.year())
+
+    @staticmethod
+    def __to_qdate(py_date: date) -> QDate:
+        return QDate(py_date.year, py_date.month, py_date.day)
