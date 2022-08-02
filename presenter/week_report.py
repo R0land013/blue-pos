8dcf11686f120d6c9a7e895769aa9c6754a87ccf
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-08-02 10:30:40 -0400

    Feat: Creada funcionalidad para crear reportes de ventas semanales en la GUI
    	Se implementó el ordenamiento de las tablas, cuando se clickea un encabezado
    	de columna.

--- presenter/week_report.py
+++ presenter/week_report.py
@@ -1,12 +1,65 @@
 from easy_mvp.abstract_presenter import AbstractPresenter
 
+from model.entity.models import Sale
+from model.report.week import WeekSaleReport
+from model.repository.factory import RepositoryFactory
+from presenter.util.thread_worker import PresenterThreadWorker
 from view.week_report import WeekSaleReportView
 
 
 class WeekSaleReportPresenter(AbstractPresenter):
 
     def _on_initialize(self):
+        self.__sale_repo = RepositoryFactory.get_sale_repository()
         self._set_view(WeekSaleReportView(self))
 
     def close_presenter(self):
         self._close_this_presenter()
+
+    def execute_thread_to_generate_report_on_gui(self):
+        self.thread = PresenterThreadWorker(self.__load_report_sales)
+        self.thread.when_started.connect(self.__disable_gui_and_show_processing_message)
+        self.thread.when_finished.connect(self.__fill_table)
+        self.thread.when_finished.connect(self.get_view().sort_table_rows)
+        self.thread.when_finished.connect(self.__set_report_statistics)
+        self.thread.when_finished.connect(self.__set_available_gui_and_show_no_state_bar_message)
+        self.thread.start()
+
+    def __load_report_sales(self, thread: PresenterThreadWorker):
+        initial_date, final_date = self.get_view().get_limit_dates_of_week()
+        self.__week_report = WeekSaleReport(week_day=initial_date, sale_repository=self.__sale_repo)
+        self.__sales = self.__week_report.get_sales()
+
+    def __disable_gui_and_show_processing_message(self):
+        self.get_view().set_disabled_view_except_status_bar(True)
+        self.get_view().set_state_bar_message('Creando reporte...')
+
+    def __fill_table(self):
+        self.get_view().clean_table()
+        for a_sale in self.__sales:
+            self.__insert_sale_on_table(a_sale)
+        self.get_view().resize_table_columns_to_contents()
+
+    def __insert_sale_on_table(self, sale: Sale):
+        view = self.get_view()
+        view.add_empty_row_at_the_end_of_table()
+        row = view.get_last_table_row_index()
+
+        view.set_cell_on_table(row, WeekSaleReportView.SALE_ID_COLUMN, str(sale.id))
+        view.set_cell_on_table(row, WeekSaleReportView.PRODUCT_NAME_COLUMN, str(sale.product.name))
+        view.set_cell_on_table(row, WeekSaleReportView.PRODUCT_ID_COLUMN, str(sale.product.id))
+        view.set_cell_on_table(row, WeekSaleReportView.SALE_PRICE_COLUMN, str(sale.price))
+        view.set_cell_on_table(row, WeekSaleReportView.SALE_PROFIT_COLUMN, str(sale.profit))
+        view.set_cell_on_table(row, WeekSaleReportView.SALE_DATE_COLUMN, str(sale.date))
+
+    def __set_report_statistics(self):
+        report_statistics = self.__week_report.get_report_statistics()
+        self.get_view().set_initial_date(report_statistics.initial_date())
+        self.get_view().set_final_date(report_statistics.final_date())
+        self.get_view().set_sale_quantity(report_statistics.sale_quantity())
+        self.get_view().set_paid_money(str(report_statistics.paid_money()))
+        self.get_view().set_profit_money(str(report_statistics.profit_money()))
+
+    def __set_available_gui_and_show_no_state_bar_message(self):
+        self.get_view().set_disabled_view_except_status_bar(False)
+        self.get_view().set_state_bar_message('')
--- view/week_report.py
+++ view/week_report.py
@@ -1,22 +1,34 @@
-from PyQt5.QtCore import QDate
-from PyQt5.QtWidgets import QFrame, QHBoxLayout
+from datetime import date
+
+from PyQt5.QtCore import QDate, Qt
+from PyQt5.QtWidgets import QFrame, QHBoxLayout, QTableWidget, QTableWidgetItem
 from PyQt5.uic import loadUi
 
+from view.util.table_columns import QCUPMoneyTableItem, QIntegerTableItem
 from view.util.week_selector import QWeekCalendarSelectorWidget
 
 
 class WeekSaleReportView(QFrame):
 
+    SALE_ID_COLUMN = 0
+    PRODUCT_NAME_COLUMN = 1
+    PRODUCT_ID_COLUMN = 2
+    SALE_PRICE_COLUMN = 3
+    SALE_PROFIT_COLUMN = 4
+    SALE_DATE_COLUMN = 5
+
     def __init__(self, presenter):
         super().__init__()
         self.__presenter = presenter
+        self.__sorting_column = self.SALE_DATE_COLUMN
+        self.__sorting_order = Qt.AscendingOrder
 
         self.__setup_gui()
 
     def __setup_gui(self):
         loadUi('./view/ui/week_sale_report.ui', self)
         self.__setup_week_calendar_selector()
-
+        self.__setup_table()
         self.__wire_up_gui_connections()
 
     def __setup_week_calendar_selector(self):
@@ -25,5 +37,99 @@ class WeekSaleReportView(QFrame):
         self.calendar_frame.setLayout(QHBoxLayout())
         self.calendar_frame.layout().addWidget(self.__week_calendar_selector)
 
+    def __setup_table(self):
+        self.sale_report_table.setColumnCount(6)
+        self.sale_report_table.setHorizontalHeaderLabels([
+            'Id. Venta',
+            'Producto',
+            'Id. Producto',
+            'Pagado',
+            'Ganancia',
+            'Fecha'
+        ])
+        self.sale_report_table.resizeColumnsToContents()
+        self.sale_report_table.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
+        self.sale_report_table.horizontalHeader().setSectionsClickable(True)
+
     def __wire_up_gui_connections(self):
         self.back_button.clicked.connect(self.__presenter.close_presenter)
+        self.create_report_button.clicked.connect(self.__presenter.execute_thread_to_generate_report_on_gui)
+        self.sale_report_table.horizontalHeader().sectionClicked.connect(self.__change_sorting_configuration)
+        self.sale_report_table.horizontalHeader().sectionClicked.connect(self.sort_table_rows)
+
+    def __change_sorting_configuration(self, clicked_header_section: int):
+        if clicked_header_section == self.__sorting_column:
+            self.__sorting_order = (Qt.AscendingOrder if self.__sorting_order == Qt.DescendingOrder
+                                    else Qt.DescendingOrder)
+        else:
+            self.__sorting_column = clicked_header_section
+            self.__sorting_order = Qt.AscendingOrder
+
+    def sort_table_rows(self):
+        horizontal_header = self.sale_report_table.horizontalHeader()
+        horizontal_header.setSortIndicator(self.__sorting_column, self.__sorting_order)
+        horizontal_header.setSortIndicatorShown(True)
+        self.sale_report_table.sortItems(self.__sorting_column, self.__sorting_order)
+
+    def get_limit_dates_of_week(self) -> tuple:
+        initial_qdate, final_qdate = self.__week_calendar_selector.get_selected_date_range()
+        initial_py_date = date(day=initial_qdate.day(),
+                               month=initial_qdate.month(),
+                               year=initial_qdate.year())
+        final_py_date = date(day=final_qdate.day(),
+                             month=final_qdate.month(),
+                             year=final_qdate.year())
+        return initial_py_date, final_py_date
+
+    def set_initial_date(self, initial_date: date):
+        self.initial_date_label.setText(
+            '{}/{}/{}'.format(
+                initial_date.day,
+                initial_date.month,
+                initial_date.year))
+
+    def set_final_date(self, final_date: date):
+        self.final_date_label.setText(
+            '{}/{}/{}'.format(
+                final_date.day,
+                final_date.month,
+                final_date.year))
+
+    def set_sale_quantity(self, quantity: int):
+        self.sale_quantity_label.setText(str(quantity))
+
+    def set_paid_money(self, paid: str):
+        self.payment_money_label.setText(paid)
+
+    def set_profit_money(self, profit: str):
+        self.profit_money_label.setText(profit)
+
+    def set_disabled_view_except_status_bar(self, disable: bool):
+        self.main_content_frame.setDisabled(disable)
+        self.export_as_button.setDisabled(disable)
+
+    def set_state_bar_message(self, message: str):
+        self.state_bar_label.setText(message)
+
+    def clean_table(self):
+        while self.sale_report_table.rowCount() > 0:
+            self.sale_report_table.removeRow(0)
+
+    def add_empty_row_at_the_end_of_table(self):
+        new_row_index = self.sale_report_table.rowCount()
+        self.sale_report_table.insertRow(new_row_index)
+
+    def set_cell_on_table(self, row: int, column: int, data):
+
+        item = QTableWidgetItem(str(data))
+        if column == self.SALE_PRICE_COLUMN or column == self.SALE_PROFIT_COLUMN:
+            item = QCUPMoneyTableItem(str(data))
+        elif column == self.SALE_ID_COLUMN or column == self.PRODUCT_ID_COLUMN:
+            item = QIntegerTableItem(str(data))
+        self.sale_report_table.setItem(row, column, item)
+
+    def get_last_table_row_index(self) -> int:
+        return self.sale_report_table.rowCount() - 1
+
+    def resize_table_columns_to_contents(self):
+        self.sale_report_table.resizeColumnsToContents()
