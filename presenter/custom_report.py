5bb4e0abfbaa88a2b34b4d1d4689abcbf1a6e45b
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-08-03 10:27:41 -0400

    Feat: Configurados los campos de nombre, descripción, fecha inicial, fecha final, ordenar por y orden de la vista de reporte personalizado

--- presenter/custom_report.py
+++ presenter/custom_report.py
@@ -1,5 +1,6 @@
 from easy_mvp.abstract_presenter import AbstractPresenter
 
+from model.repository.sale import SaleFilter
 from view.custom_report import CustomSaleReportView
 
 
@@ -10,3 +11,39 @@ class CustomSaleReportPresenter(AbstractPresenter):
 
     def close_presenter(self):
         self._close_this_presenter()
+
+    def open_custom_report_visualizer_presenter(self):
+        sale_filter = self.__create_sale_filter()
+        print("""
+        name: '{}',
+        description: '{}',
+        minimum_date: {},
+        maximum_date: {},
+        sorted_by: {},
+        ascending_order: {}
+        """.format(
+            self.get_view().get_report_name(),
+            self.get_view().get_report_description(),
+            sale_filter.minimum_date,
+            sale_filter.maximum_date,
+            sale_filter.sorted_by,
+            sale_filter.ascending_order
+        ))
+
+    def __create_sale_filter(self) -> SaleFilter:
+        sale_filter = SaleFilter()
+        sale_filter.minimum_date = self.get_view().get_initial_date()
+        sale_filter.maximum_date = self.get_view().get_final_date()
+        sale_filter.sorted_by = self.__get_column_to_order_sale_report()
+        sale_filter.ascending_order = self.get_view().is_ascending_order()
+        return sale_filter
+
+    def __get_column_to_order_sale_report(self):
+        column = self.get_view().get_order_by_report_column()
+        if column == CustomSaleReportView.SALE_ID_REPORT_COLUMN:
+            return SaleFilter.ID
+        elif column == CustomSaleReportView.PAID_REPORT_COLUMN:
+            return SaleFilter.PRICE
+        elif column == CustomSaleReportView.PROFIT_REPORT_COLUMN:
+            return SaleFilter.PROFIT
+        return SaleFilter.SALE_DATE
--- view/custom_report.py
+++ view/custom_report.py
@@ -1,8 +1,16 @@
+from datetime import date
+
+from PyQt5.QtCore import QDate
 from PyQt5.QtWidgets import QFrame
 from PyQt5.uic import loadUi
 
 
 class CustomSaleReportView(QFrame):
+
+    SALE_ID_REPORT_COLUMN = 'Id. de Venta'
+    PAID_REPORT_COLUMN = 'Pagado'
+    PROFIT_REPORT_COLUMN = 'Ganancia'
+    SALE_DATE_REPORT_COLUMN = 'Fecha'
 
     def __init__(self, presenter):
         super().__init__()
@@ -12,7 +20,58 @@ class CustomSaleReportView(QFrame):
 
     def __setup_gui(self):
         loadUi('./view/ui/custom_sale_report.ui', self)
+        self.__setup_date_edit()
+        self.__setup_order_by_combo_box()
         self.__wire_up_gui_connections()
+
+        self.state_bar_label.hide()
+
+    def __setup_date_edit(self):
+        self.initial_date_edit.setDate(QDate.currentDate())
+        self.final_date_edit.setDate(QDate.currentDate())
+        self.initial_date_edit.setMaximumDate(QDate.currentDate())
+        self.final_date_edit.setMaximumDate(QDate.currentDate())
+        self.initial_date_edit.lineEdit().setReadOnly(True)
+        self.final_date_edit.lineEdit().setReadOnly(True)
+        self.final_date_edit.dateChanged.connect(
+            lambda: self.initial_date_edit.setMaximumDate(self.final_date_edit.date())
+        )
+
+    def __setup_order_by_combo_box(self):
+        report_columns = [
+            self.SALE_ID_REPORT_COLUMN,
+            self.PAID_REPORT_COLUMN,
+            self.PROFIT_REPORT_COLUMN,
+            self.SALE_DATE_REPORT_COLUMN
+        ]
+        self.order_by_combo_box.addItems(report_columns)
 
     def __wire_up_gui_connections(self):
         self.back_button.clicked.connect(self.__presenter.close_presenter)
+        self.create_report_button.clicked.connect(self.__presenter.open_custom_report_visualizer_presenter)
+
+    def get_report_name(self) -> str:
+        return self.name_line_edit.text().lstrip().strip()
+
+    def get_report_description(self) -> str:
+        return self.description_plain_text_edit.document().toRawText().lstrip().rstrip()
+
+    def get_initial_date(self) -> date:
+        qdate: QDate = self.initial_date_edit.date()
+        return date(day=qdate.day(),
+                    month=qdate.month(),
+                    year=qdate.year())
+
+    def get_final_date(self) -> date:
+        qdate: QDate = self.final_date_edit.date()
+        return date(day=qdate.day(),
+                    month=qdate.month(),
+                    year=qdate.year())
+
+    def get_order_by_report_column(self) -> str:
+        return self.order_by_combo_box.currentText()
+
+    def is_ascending_order(self) -> bool:
+        if self.ascending_radio_button.isChecked():
+            return True
+        return False
