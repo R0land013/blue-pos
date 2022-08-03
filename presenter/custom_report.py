da93dc55a1b3bef2d7474434da2b2844bd37a6c2
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-08-03 11:35:11 -0400

    Feat: Implementada la funcionalidad del botón 'Incluir todos' los productos en la vista de reporte personalizada
    	EL QListWidget que iba a contener los productos fue reemplazado por un QTableWidget en el archivo
    	custom_sale_report.ui. Esto se hizo con la finalidad de que el usuario pudiese ordenar, buscar y
    	visualizar rápidamente los productos seleccionados para el reporte personalizado.

--- presenter/custom_report.py
+++ presenter/custom_report.py
@@ -1,6 +1,9 @@
 from easy_mvp.abstract_presenter import AbstractPresenter
 
+from model.entity.models import Product
+from model.repository.factory import RepositoryFactory
 from model.repository.sale import SaleFilter
+from presenter.util.thread_worker import PresenterThreadWorker
 from view.custom_report import CustomSaleReportView
 
 
@@ -8,6 +11,8 @@ class CustomSaleReportPresenter(AbstractPresenter):
 
     def _on_initialize(self):
         self._set_view(CustomSaleReportView(self))
+        self.__product_repo = RepositoryFactory.get_product_repository()
+        self.__selected_products = []
 
     def close_presenter(self):
         self._close_this_presenter()
@@ -19,6 +24,7 @@ class CustomSaleReportPresenter(AbstractPresenter):
         description: '{}',
         minimum_date: {},
         maximum_date: {},
+        product_id_list: {},
         sorted_by: {},
         ascending_order: {}
         """.format(
@@ -26,6 +32,7 @@ class CustomSaleReportPresenter(AbstractPresenter):
             self.get_view().get_report_description(),
             sale_filter.minimum_date,
             sale_filter.maximum_date,
+            sale_filter.product_id_list,
             sale_filter.sorted_by,
             sale_filter.ascending_order
         ))
@@ -36,6 +43,7 @@ class CustomSaleReportPresenter(AbstractPresenter):
         sale_filter.maximum_date = self.get_view().get_final_date()
         sale_filter.sorted_by = self.__get_column_to_order_sale_report()
         sale_filter.ascending_order = self.get_view().is_ascending_order()
+        sale_filter.product_id_list = list(map(lambda x: x.id, self.__selected_products))
         return sale_filter
 
     def __get_column_to_order_sale_report(self):
@@ -47,3 +55,37 @@ class CustomSaleReportPresenter(AbstractPresenter):
         elif column == CustomSaleReportView.PROFIT_REPORT_COLUMN:
             return SaleFilter.PROFIT
         return SaleFilter.SALE_DATE
+
+
+    def execute_thread_to_insert_all_products_on_table(self):
+        self.thread = PresenterThreadWorker(self.__load_all_products)
+        self.thread.when_started.connect(self.__disable_gui_and_show_loading_products_message)
+        self.thread.when_finished.connect(self.__fill_product_table)
+        self.thread.when_finished.connect(self.__set_all_gui_available_and_hide_state_bar)
+        self.thread.start()
+
+    def __load_all_products(self, thread: PresenterThreadWorker):
+        self.__selected_products = self.__product_repo.get_all_products()
+
+    def __disable_gui_and_show_loading_products_message(self):
+        self.get_view().disable_all_gui(True)
+        self.get_view().show_state_bar()
+        self.get_view().set_state_bar_message('Cargando productos...')
+
+    def __fill_product_table(self):
+        self.get_view().clean_table()
+        for a_product in self.__selected_products:
+            self.__add_product_to_table(a_product)
+        self.get_view().resize_table_columns_to_contents()
+
+    def __add_product_to_table(self, a_product: Product):
+        view = self.get_view()
+        view.add_empty_row_at_the_end_of_table()
+        row = view.get_last_row_index()
+
+        view.set_cell_in_table(row, CustomSaleReportView.PRODUCT_ID_TABLE_COLUMN, a_product.id)
+        view.set_cell_in_table(row, CustomSaleReportView.PRODUCT_NAME_TABLE_COLUMN, a_product.name)
+
+    def __set_all_gui_available_and_hide_state_bar(self):
+        self.get_view().disable_all_gui(False)
+        self.get_view().hide_state_bar()
--- view/custom_report.py
+++ view/custom_report.py
@@ -1,8 +1,10 @@
 from datetime import date
 
 from PyQt5.QtCore import QDate
-from PyQt5.QtWidgets import QFrame
+from PyQt5.QtWidgets import QFrame, QTableWidget, QTableWidgetItem
 from PyQt5.uic import loadUi
+
+from view.util.table_columns import QIntegerTableItem
 
 
 class CustomSaleReportView(QFrame):
@@ -11,6 +13,9 @@ class CustomSaleReportView(QFrame):
     PAID_REPORT_COLUMN = 'Pagado'
     PROFIT_REPORT_COLUMN = 'Ganancia'
     SALE_DATE_REPORT_COLUMN = 'Fecha'
+
+    PRODUCT_ID_TABLE_COLUMN = 0
+    PRODUCT_NAME_TABLE_COLUMN = 1
 
     def __init__(self, presenter):
         super().__init__()
@@ -22,9 +27,10 @@ class CustomSaleReportView(QFrame):
         loadUi('./view/ui/custom_sale_report.ui', self)
         self.__setup_date_edit()
         self.__setup_order_by_combo_box()
+        self.__setup_product_table()
         self.__wire_up_gui_connections()
 
-        self.state_bar_label.hide()
+        self.hide_state_bar()
 
     def __setup_date_edit(self):
         self.initial_date_edit.setDate(QDate.currentDate())
@@ -46,9 +52,21 @@ class CustomSaleReportView(QFrame):
         ]
         self.order_by_combo_box.addItems(report_columns)
 
+    def __setup_product_table(self):
+        self.selected_products_table.setColumnCount(2)
+        self.selected_products_table.setHorizontalHeaderLabels([
+            'Id.',
+            'Producto'
+        ])
+        self.selected_products_table.resizeColumnsToContents()
+        self.selected_products_table.setSelectionBehavior(QTableWidget.SelectionBehavior.SelectRows)
+        self.selected_products_table.setSelectionMode(QTableWidget.SelectionMode.NoSelection)
+        self.selected_products_table.setSortingEnabled(True)
+
     def __wire_up_gui_connections(self):
         self.back_button.clicked.connect(self.__presenter.close_presenter)
         self.create_report_button.clicked.connect(self.__presenter.open_custom_report_visualizer_presenter)
+        self.include_all_button.clicked.connect(self.__presenter.execute_thread_to_insert_all_products_on_table)
 
     def get_report_name(self) -> str:
         return self.name_line_edit.text().lstrip().strip()
@@ -75,3 +93,42 @@ class CustomSaleReportView(QFrame):
         if self.ascending_radio_button.isChecked():
             return True
         return False
+
+    def disable_all_gui(self, disable: bool):
+        self.main_content_frame.setDisabled(disable)
+        self.back_button.setDisabled(disable)
+        self.create_report_button.setDisabled(disable)
+
+    def hide_state_bar(self):
+        self.state_bar_label.hide()
+
+    def show_state_bar(self):
+        self.state_bar_label.show()
+
+    def set_state_bar_message(self, text: str):
+        self.state_bar_label.setText(text)
+
+    def clean_table(self):
+        while self.selected_products_table.rowCount() > 0:
+            self.selected_products_table.removeRow(0)
+
+    def add_empty_row_at_the_end_of_table(self):
+        new_row_index = self.selected_products_table.rowCount()
+        self.selected_products_table.insertRow(new_row_index)
+
+    def get_last_row_index(self) -> int:
+        return self.selected_products_table.rowCount() - 1
+
+    def set_cell_in_table(self, row: int, column: int, data):
+
+        item = QTableWidgetItem(str(data))
+        if column == self.PRODUCT_ID_TABLE_COLUMN:
+            item = QIntegerTableItem(str(data))
+
+        self.selected_products_table.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
+        self.selected_products_table.setItem(row, column, item)
+
+    def resize_table_columns_to_contents(self):
+        self.selected_products_table.resizeColumnsToContents()
+
+
