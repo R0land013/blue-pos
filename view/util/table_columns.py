f2b7b8ebf1aee033cc8f06370fc87764788f915b
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-08-05 18:09:40 -0400

    Fix: Arreglo en QCUPMoneyTableItem ya que lanzaba un error cuando se ordenaba una columna con ese tipo de datos
    	El problema estaba en que cuando se introduce el Money a la tabla se hace de la manera
    	str(money_object), imprimiendos con comas cada tres lugares no decimales, por ejemplo
    	'CUP 1,000.00'. Sin embargo a la hora de construir un nuevo objeto Money para realizar
    	el ordenamiento de una columna de la tabla no se puede pasar cadenas con comas pues la
    	clase base para representar las cantidades en la calse Money, Decimal, no acepta cadenas
    	con comas.

--- view/util/table_columns.py
+++ view/util/table_columns.py
@@ -14,9 +14,19 @@ class QCUPMoneyTableItem(QTableWidgetItem):
         super().__init__(text, type=QCustomTableItemTypes.CUP_MONEY_TYPE)
 
     def __lt__(self, other):
-        this_money = CUPMoney(self.text().split()[1])
-        other_money = CUPMoney(other.text().split()[1])
+        this_amount_str = self.text().split()[1]
+        other_amount_str = other.text().split()[1]
+        this_money = self.__create_cup_money(this_amount_str)
+        other_money = self.__create_cup_money(other_amount_str)
         return this_money < other_money
+
+    @staticmethod
+    def __create_cup_money(cell_text: str):
+        # CUPMoney usa comas para separar tres lugares en los números
+        # cuando se imprimen. Por esta razón es necesario eliminar las comas
+        # de el número para crear el objeto Money. Ejemplo: CUPMoney('1,000.00')
+        # es incorrecto.  La manera correcta es CUPMoney('1000.00'), sin comas.
+        return CUPMoney(''.join(cell_text.split(',')))
 
 
 class QIntegerTableItem(QTableWidgetItem):
