b3394cba57842959c4cf261298f9fa4ed768e9dc
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-08-11 11:33:50 -0400

    Style: Los botones de navegación de la vista principal tienen un label que indican hacia dónde van

--- view/main.py
+++ view/main.py
@@ -9,29 +9,22 @@ from view.util.animations import GoForwardAndReturnAnimation
 
 class MainView(QFrame):
 
-    clicked_on_next_label = pyqtSignal()
-    clicked_on_previous_label = pyqtSignal()
+    clicked_on_next_frame = pyqtSignal()
+    clicked_on_previous_frame = pyqtSignal()
 
     def __init__(self, presenter):
         super().__init__()
         self.__presenter = presenter
-        self.__next_label_animation = None
-        self.__previous_label_animation = None
+        self.__next_frame_animation = None
+        self.__previous_frame_animation = None
 
         self.__set_up_gui()
 
     def __set_up_gui(self):
         loadUi('./view/ui/main.ui', self)
-        self.__set_available_mouse_tracking()
         self.wire_up_gui_connections()
 
-        self.previous_label.hide()  # Para que la navegación hacia la izquierda no aparezca
-
-    def __set_available_mouse_tracking(self):
-        self.setMouseTracking(True)
-        self.next_label.setMouseTracking(True)
-        self.previous_label.setMouseTracking(True)
-        self.stacked_widget.setMouseTracking(True)
+        self.previous_frame.hide()  # Para que la navegación hacia la izquierda no aparezca
 
     def wire_up_gui_connections(self):
         self.product_management_button.clicked.connect(self.__presenter.open_product_management)
@@ -40,51 +33,51 @@ class MainView(QFrame):
         self.year_report_button.clicked.connect(self.__presenter.open_year_sale_report_presenter)
         self.week_report_button.clicked.connect(self.__presenter.open_week_sale_report_presenter)
         self.custom_report_button.clicked.connect(self.__presenter.open_custom_sale_report_presenter)
-        self.clicked_on_next_label.connect(self.__show_reports_widget)
-        self.clicked_on_previous_label.connect(self.__show_management_widget)
+        self.clicked_on_next_frame.connect(self.__show_reports_widget)
+        self.clicked_on_previous_frame.connect(self.__show_management_widget)
 
     def __show_reports_widget(self):
-        self.next_label.hide()
-        self.previous_label.show()
+        self.next_frame.hide()
+        self.previous_frame.show()
         self.stacked_widget.setCurrentWidget(self.reports_widget)
 
     def __show_management_widget(self):
-        self.previous_label.hide()
-        self.next_label.show()
+        self.previous_frame.hide()
+        self.next_frame.show()
         self.stacked_widget.setCurrentWidget(self.management_widget)
 
     def mouseMoveEvent(self, event: QMouseEvent):
         self.__create_animations()
-        self.__start_next_label_animation_if_mouse_is_hovering()
-        self.__start_previous_label_animation_if_mouse_is_hovering()
+        self.__start_next_frame_animation_if_mouse_is_hovering()
+        self.__start_previous_frame_animation_if_mouse_is_hovering()
 
     def __create_animations(self):
-        if self.__next_label_animation is None:
-            self.__next_label_animation = GoForwardAndReturnAnimation(
-                self.next_label, b'pos', 30, GoForwardAndReturnAnimation.RIGHT_DIRECTION)
-            self.__next_label_animation.finished.connect(
-                self.__start_next_label_animation_if_mouse_is_hovering
+        if self.__next_frame_animation is None:
+            self.__next_frame_animation = GoForwardAndReturnAnimation(
+                self.next_frame, b'pos', 30, GoForwardAndReturnAnimation.RIGHT_DIRECTION)
+            self.__next_frame_animation.finished.connect(
+                self.__start_next_frame_animation_if_mouse_is_hovering
             )
 
-        if self.__previous_label_animation is None:
-            self.__previous_label_animation = GoForwardAndReturnAnimation(
-                self.previous_label, b'pos', 30, GoForwardAndReturnAnimation.LEFT_DIRECTION
+        if self.__previous_frame_animation is None:
+            self.__previous_frame_animation = GoForwardAndReturnAnimation(
+                self.previous_frame, b'pos', 30, GoForwardAndReturnAnimation.LEFT_DIRECTION
             )
-            self.__previous_label_animation.finished.connect(
-                self.__start_previous_label_animation_if_mouse_is_hovering)
+            self.__previous_frame_animation.finished.connect(
+                self.__start_previous_frame_animation_if_mouse_is_hovering)
 
-    def __start_next_label_animation_if_mouse_is_hovering(self):
-        if self.next_label.underMouse() and (
-                self.__next_label_animation.state() == QPropertyAnimation.Stopped):
-            self.__next_label_animation.start()
+    def __start_next_frame_animation_if_mouse_is_hovering(self):
+        if self.next_frame.underMouse() and (
+                self.__next_frame_animation.state() == QPropertyAnimation.Stopped):
+            self.__next_frame_animation.start()
 
-    def __start_previous_label_animation_if_mouse_is_hovering(self):
-        if self.previous_label.underMouse() and (
-                self.__previous_label_animation.state() == QPropertyAnimation.Stopped):
-            self.__previous_label_animation.start()
+    def __start_previous_frame_animation_if_mouse_is_hovering(self):
+        if self.previous_frame.underMouse() and (
+                self.__previous_frame_animation.state() == QPropertyAnimation.Stopped):
+            self.__previous_frame_animation.start()
 
     def mouseReleaseEvent(self, mouse_event: QMouseEvent):
-        if self.next_label.underMouse():
-            self.clicked_on_next_label.emit()
-        elif self.previous_label.underMouse():
-            self.clicked_on_previous_label.emit()
+        if self.next_frame.underMouse():
+            self.clicked_on_next_frame.emit()
+        elif self.previous_frame.underMouse():
+            self.clicked_on_previous_frame.emit()
