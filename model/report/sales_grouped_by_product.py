7c7f8a0e012b4d69762bf2d72c637a972a0954c5
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-08-27 14:18:02 -0400

    Feat: Implementado SalesGroupedByProductRepository.get_groups_by_day_on_week
    	El tests que verifica este método se ejecutó sin fallas

--- model/report/sales_grouped_by_product.py
+++ model/report/sales_grouped_by_product.py
@@ -7,7 +7,6 @@ class SalesGroupedByProduct:
     def __init__(self, product_id: int, product_name: str, sale_quantity: int,
                  acquired_money: Money, total_cost: Money, total_profit: Money,
                  initial_date: date, final_date: date):
-
         self.__product_id: int = product_id
         self.__product_name: str = product_name
         self.__sale_quantity: int = sale_quantity
@@ -16,6 +15,28 @@ class SalesGroupedByProduct:
         self.__total_profit: Money = total_profit
         self.__initial_date: date = initial_date
         self.__final_date: date = final_date
+
+    def __str__(self):
+        return 'SalesGroupedByProduct(product_id: {}, product_name: \'{}\', ' \
+               'sale_quantity: {}, acquired_money: {}, total_cost: {}, ' \
+               'total_profit: {}, initial_date: {}, final_date: {})'.format(
+                self.__product_id, self.__product_name, self.__sale_quantity,
+                self.__acquired_money, self.__total_cost, self.__total_profit,
+                self.__initial_date, self.__final_date
+                )
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other) -> bool:
+        return (self.__product_id == other.product_id
+                and self.__product_name == other.product_name
+                and self.__sale_quantity == other.sale_quantity
+                and self.__acquired_money == other.acquired_money
+                and self.__total_cost == other.total_cost
+                and self.__total_profit == other.total_profit
+                and self.__initial_date == other.initial_date
+                and self.__final_date == other.final_date)
 
     @property
     def product_id(self) -> int:
--- model/repository/sales_grouped_by_product.py
+++ model/repository/sales_grouped_by_product.py
@@ -1,7 +1,12 @@
-from datetime import date
-from sqlalchemy.orm import Session
+from datetime import date, timedelta
+from sqlalchemy import select, cast, FLOAT
+from sqlalchemy.orm import Session, aliased
 from typing import List
+from model.entity.models import Product, Sale
 from model.report.sales_grouped_by_product import SalesGroupedByProduct
+from sqlalchemy import func
+
+from model.util.monetary_types import CUPMoney
 
 
 class SalesGroupedByProductRepository:
@@ -10,7 +15,42 @@ class SalesGroupedByProductRepository:
         self.__session: Session = session
 
     def get_groups_by_day_on_week(self, week_date: date) -> List[SalesGroupedByProduct]:
-        pass
+        monday_date = week_date - timedelta(days=week_date.weekday())
+        sunday_date = week_date + timedelta(days=6 - week_date.weekday())
+        query = self.__construct_query_using_date_limits(monday_date, sunday_date)
+        rows = self.__session.execute(query).all()
+        return self.__construct_sale_groups_from_result(rows, monday_date, sunday_date)
+
+    @staticmethod
+    def __construct_query_using_date_limits(initial_date: date, final_date: date):
+        acquired_money = func.sum(cast(Sale.price, FLOAT)).label('acquired_money')
+        total_cost = func.sum(cast(Sale.cost, FLOAT)).label('total_cost')
+        sale_quantity = func.count(Sale.id).label('sale_quantity')
+        query = select(Product.id, Product.name, acquired_money, total_cost, sale_quantity)\
+            .join(Sale, Product.id == Sale.product_id)\
+            .where(Sale.date >= initial_date)\
+            .where(Sale.date <= final_date)\
+            .group_by(Sale.product_id)
+        return query
+
+    @staticmethod
+    def __construct_sale_groups_from_result(rows, initial_date: date, final_date: date) -> List[SalesGroupedByProduct]:
+        groups = []
+        for a_row in rows:
+            acquired_money = CUPMoney(str(a_row['acquired_money']))
+            total_cost = CUPMoney(str(a_row['total_cost']))
+            total_profit = acquired_money - total_cost
+            groups.append(SalesGroupedByProduct(
+                product_id=a_row[0],
+                product_name=a_row[1],
+                acquired_money=acquired_money,
+                total_cost=total_cost,
+                total_profit=total_profit,
+                sale_quantity=a_row[4],
+                initial_date=initial_date,
+                final_date=final_date
+            ))
+        return groups
 
     def get_groups_by_day_on_month(self, month_date: date) -> List[SalesGroupedByProduct]:
         pass
