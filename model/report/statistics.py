d4c1a43f0127a6f0a238073d2bdaed25e6e768c6
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-08-30 09:18:58 -0400

    Test: Creada prueba para comprobar que las estadísticas del reporte semanal se crean correctamente
    	La prueba se ejecutó sin fallas.
    
    	Feat: Se implementó __str__, __repr__ y __eq__ de ReportStatistic.
    
    	Muchos tests se rompieron cuando se cambió el constructor de ReportStatistic
    	en el commit anterior.

--- model/report/statistics.py
+++ model/report/statistics.py
@@ -13,6 +13,21 @@ class ReportStatistic:
         self.__initial_date = initial_date
         self.__final_date = final_date
 
+    def __eq__(self, other):
+        return (self.__sale_quantity == other.sale_quantity() and self.__paid_money == other.paid_money()
+                and self.__cost_money == other.cost_money() and self.__total_expenses == other.total_expenses()
+                and self.__initial_date == other.initial_date() and self.__final_date == other.final_date())
+
+    def __str__(self):
+        return 'ReportStatistic( sale_quantity: {}, paid_money: {}, cost_money: {}, profit_money: {},' \
+               'total_expenses: {}, net_profit: {}, initial_date: {}, final_date: {})'.format(
+                self.__sale_quantity, self.__paid_money, self.__cost_money, self.profit_money(),
+                self.__total_expenses, self.net_profit(), self.__initial_date, self.__final_date
+                )
+
+    def __repr__(self):
+        return self.__str__()
+
     def sale_quantity(self) -> int:
         return self.__sale_quantity
 
