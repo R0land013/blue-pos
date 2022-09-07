3ea8c646a2c2fafd090fd327aa9e0fc142333187
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-09-07 17:51:37 -0400

    Feat: Se puede ver el gráfico de las ganancias netas anuales en la vista de estadísticas anuales
    	El gráfico todavía no es interactivo, para mostrar los valores asociados con cada punto.

--- presenter/year_statistics.py
+++ presenter/year_statistics.py
@@ -1,5 +1,10 @@
+from datetime import date
+from typing import Tuple
 from easy_mvp.abstract_presenter import AbstractPresenter
 
+from model.entity.economic_summary import EconomicSummary
+from model.repository.factory import RepositoryFactory
+from presenter.util.thread_worker import PresenterThreadWorker
 from view.year_statistics import YearStatisticsView
 
 
@@ -7,9 +12,41 @@ class YearStatisticsPresenter(AbstractPresenter):
 
     def _on_initialize(self):
         self._set_view(YearStatisticsView(self))
+        self.__summary_repo = RepositoryFactory.get_economic_summary_repository()
+        self.__selected_year: int = 2000
+        self.__month_summaries: Tuple[EconomicSummary] = None
 
     def close_presenter(self):
         self._close_this_presenter()
 
     def get_default_window_title(self) -> str:
         return 'Blue POS - Estadísticas Anuales'
+
+    def calculate_economic_month_summaries(self):
+        self.__selected_year = self.get_view().get_selected_year()
+        self.thread = PresenterThreadWorker(self.__load_summaries)
+
+        self.thread.when_started.connect(lambda: self.get_view().disable_gui(True))
+        self.thread.when_started.connect(lambda: self.get_view().set_status_bar_message('Calculando...'))
+
+        self.thread.when_finished.connect(self.__plot_summaries_on_graph)
+        self.thread.when_finished.connect(lambda: self.get_view().disable_gui(False))
+        self.thread.when_started.connect(lambda: self.get_view().set_status_bar_message(''))
+
+        self.thread.start()
+
+    def __load_summaries(self, thread: PresenterThreadWorker):
+        summaries_list = []
+        current_month = 1
+        while current_month <= 12:
+            month_date = date(day=1, month=current_month, year=self.__selected_year)
+            summaries_list.append(self.__summary_repo.get_economic_summary_on_month(month_date))
+
+            current_month += 1
+        self.__month_summaries = tuple(summaries_list)
+
+    def __plot_summaries_on_graph(self):
+        months_values = list(map(lambda summary: summary.initial_date.month, self.__month_summaries))
+        net_profit_values = list(map(lambda summary: float(summary.net_profit.amount), self.__month_summaries))
+
+        self.get_view().plot_values(month_axis=months_values, y_axis=net_profit_values)
--- view/year_statistics.py
+++ view/year_statistics.py
@@ -1,5 +1,9 @@
+from datetime import date
+
+from PyQt5.QtCore import QDate
 from PyQt5.QtWidgets import QFrame
 from PyQt5.uic import loadUi
+from pyqtgraph import PlotWidget, PlotItem, AxisItem
 
 
 class YearStatisticsView(QFrame):
@@ -12,8 +16,57 @@ class YearStatisticsView(QFrame):
 
     def __setup_gui(self):
         loadUi('./view/ui/year_statistics.ui', self)
-
+        self.__setup_date_edit()
+        self.__setup_graph()
         self.__setup_gui_connections()
+        self.set_status_bar_message('')
+
+    def __setup_date_edit(self):
+        self.year_date_edit.setMaximumDate(QDate.currentDate())
+        self.year_date_edit.setDate(QDate.currentDate())
+
+    def __setup_graph(self):
+        layout = self.graph_frame.layout()
+        self.__plot_widget = PlotWidget(parent=self.graph_frame, background='white')
+        layout.addWidget(self.__plot_widget)
+        self.__plot_widget.getPlotItem().setTitle('Ganancias Anuales')
+        self.__set_month_axis_on_plot_item()
+        self.__set_axis_limits()
+        self.__plot_widget.getPlotItem().getViewBox().setMouseEnabled(x=False, y=False)
+        self.__plot_widget.getPlotItem().getViewBox().setMenuEnabled(False)
+
+    def __set_month_axis_on_plot_item(self):
+        months_dict = {1: 'Enero', 2: 'Febrero', 3: 'Marzo', 4: 'Abril', 5: 'Mayo', 6: 'Junio',
+                       7: 'Julio', 8: 'Agosto', 9: 'Septiembre', 10: 'Octubre', 11: 'Noviembre',
+                       12: 'Diciembre'}
+        month_axis = AxisItem(orientation='bottom')
+        month_axis.setTicks([months_dict.items()])
+        plot_item: PlotItem = self.__plot_widget.getPlotItem()
+        plot_item.setAxisItems(axisItems={'bottom': month_axis})
+
+    def __set_axis_limits(self, min_y: int = 0, max_y: int = 1000):
+        self.__plot_widget.getPlotItem().getViewBox().setRange(
+            xRange=(1, 12),  # Enero y Diciembre
+            yRange=(min_y, max_y)
+        )
 
     def __setup_gui_connections(self):
         self.back_button.clicked.connect(self.__presenter.close_presenter)
+        self.calculate_button.clicked.connect(self.__presenter.calculate_economic_month_summaries)
+
+    def get_selected_year(self) -> int:
+        return self.year_date_edit.date().year()
+
+    def disable_gui(self, disable: bool):
+        self.main_content_frame.setDisabled(disable)
+        self.tool_bar_frame.setDisabled(disable)
+
+    def set_status_bar_message(self, message: str):
+        self.status_bar_label.setText(message)
+
+    def plot_values(self, month_axis: list, y_axis: list):
+        plot_item: PlotItem = self.__plot_widget.getPlotItem()
+        plot_item.clear()
+        self.__set_axis_limits(min(y_axis), max(y_axis))
+        plot_item.plot(month_axis, y_axis, symbol='o')
+
