7e8a09079137333aa03a7f7f34e6028f2cf4277f
Rolando Juan Rio Garaboa <rolandorio06@gmail.com>
2022-09-10 11:39:57 -0400

    Feat: Se calcula la gráfica de ganancia neta en la vista de estadística mensual

--- presenter/month_statistics.py
+++ presenter/month_statistics.py
@@ -1,5 +1,11 @@
+from datetime import date, timedelta
+from typing import Tuple
+
 from easy_mvp.abstract_presenter import AbstractPresenter
 
+from model.entity.economic_summary import EconomicSummary
+from model.repository.factory import RepositoryFactory
+from presenter.util.thread_worker import PresenterThreadWorker
 from view.month_statistics import MonthStatisticsView
 
 
@@ -7,9 +13,42 @@ class MonthStatisticsPresenter(AbstractPresenter):
 
     def _on_initialize(self):
         self._set_view(MonthStatisticsView(self))
+        self.__selected_month_date: date = None
+        self.__summary_repo = RepositoryFactory.get_economic_summary_repository()
+        self.__day_summaries: Tuple[EconomicSummary] = None
 
     def close_presenter(self):
         self._close_this_presenter()
 
     def get_default_window_title(self) -> str:
         return 'Blue POS - Estadísticas Mensuales'
+
+    def calculate_economic_day_summaries(self):
+        self.__selected_month_date = self.get_view().get_selected_month_date()
+        self.thread = PresenterThreadWorker(self.__load_day_summaries)
+
+        self.thread.when_started.connect(lambda: self.get_view().disable_gui(True))
+        self.thread.when_started.connect(lambda: self.get_view().set_status_bar_message('Calculando...'))
+
+        self.thread.when_finished.connect(self.__plot_summaries_on_graph)
+        self.thread.when_started.connect(lambda: self.get_view().disable_gui(False))
+        self.thread.when_started.connect(lambda: self.get_view().set_status_bar_message(''))
+
+        self.thread.start()
+
+    def __load_day_summaries(self, thread: PresenterThreadWorker):
+        current_date = date(day=1, month=self.__selected_month_date.month, year=self.__selected_month_date.year)
+        summaries_list = []
+
+        while current_date.month == self.__selected_month_date.month and current_date <= date.today():
+
+            summaries_list.append(self.__summary_repo.get_economic_summary_on_day(current_date))
+            current_date = current_date + timedelta(days=1)
+
+        self.__day_summaries = tuple(summaries_list)
+
+    def __plot_summaries_on_graph(self):
+        days = list(map(lambda summary: summary.initial_date.day, self.__day_summaries))
+        net_profit_values = list(map(lambda summary: float(summary.net_profit.amount), self.__day_summaries))
+
+        self.get_view().plot_values(x_days_values=days, y_values=net_profit_values)
--- view/month_statistics.py
+++ view/month_statistics.py
@@ -1,5 +1,9 @@
+from datetime import date, timedelta
+
+from PyQt5.QtCore import QDate
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QFrame, QToolBar, QHBoxLayout
+from pyqtgraph import PlotWidget, mkPen, AxisItem, PlotCurveItem, ScatterPlotItem, mkBrush
 from qtpy.uic import loadUi
 
 from view.util.text_tool_button import ToolButtonWithTextAndIcon
@@ -10,12 +14,15 @@ class MonthStatisticsView(QFrame):
     def __init__(self, presenter):
         super().__init__()
         self.__presenter = presenter
+        self.__calculated_month_date: date = None
 
         self.__setup_gui()
 
     def __setup_gui(self):
         loadUi('./view/ui/month_statistics.ui', self)
         self.__setup_back_tool_button()
+        self.__setup_date_edit()
+        self.__setup_graph()
         self.__setup_gui_connections()
 
     def __setup_back_tool_button(self):
@@ -28,5 +35,106 @@ class MonthStatisticsView(QFrame):
         self.back_button.set_icon(QPixmap('./view/ui/images/back.png'))
         self.tool_bar.addWidget(self.back_button)
 
+    def __setup_date_edit(self):
+        self.month_date_edit.setMaximumDate(QDate.currentDate())
+        self.month_date_edit.setDate(QDate.currentDate())
+
+    def __setup_graph(self):
+        layout = self.graph_frame.layout()
+        self.plot_widget = PlotWidget(parent=self.graph_frame, background='white', foreground='black')
+        layout.addWidget(self.plot_widget)
+        self.__set_axis_style()
+        self.plot_widget.getPlotItem().getViewBox().setMouseEnabled(x=False, y=False)
+        self.plot_widget.getPlotItem().getViewBox().setMenuEnabled(False)
+        self.plot_widget.getPlotItem().hideButtons()
+
+    def __set_axis_style(self):
+        black_pen = mkPen(color='black')
+
+        bottom_axis: AxisItem = self.plot_widget.getPlotItem().getAxis('bottom')
+        bottom_axis.setPen(black_pen)
+        bottom_axis.setTextPen(black_pen)
+
+        left_axis: AxisItem = self.plot_widget.getPlotItem().getAxis('left')
+        left_axis.setPen(black_pen)
+        left_axis.setTextPen(black_pen)
+
     def __setup_gui_connections(self):
         self.back_button.clicked.connect(self.__presenter.close_presenter)
+        self.calculate_button.clicked.connect(self.__set_days_of_month_on_bottom_axis)
+        self.calculate_button.clicked.connect(self.__presenter.calculate_economic_day_summaries)
+
+    def __set_days_of_month_on_bottom_axis(self):
+        self.__set_calculated_month_date()
+        bottom_axis: AxisItem = self.plot_widget.getPlotItem().getAxis('bottom')
+        bottom_axis.setTicks(self.__generate_day_of_month_ticks())
+        self.plot_widget.getPlotItem().getViewBox().setRange(
+            xRange=(1, self.__get_last_day_of_month())
+        )
+
+    def __set_calculated_month_date(self):
+        qdate: QDate = self.month_date_edit.date()
+        self.__calculated_month_date = date(day=1, month=qdate.month(), year=qdate.year())
+
+    def __generate_day_of_month_ticks(self):
+        ticks = {}
+        current_date = self.__calculated_month_date
+
+        while current_date.month == self.__calculated_month_date.month:
+            ticks[current_date.day] = str(current_date.day)
+
+            current_date = current_date + timedelta(days=1)
+        return [ticks.items()]
+
+    def __get_last_day_of_month(self) -> int:
+        if self.__calculated_month_date.month == 12:
+            next_month = 1
+        else:
+            next_month = self.__calculated_month_date.month + 1
+
+        if next_month == 1:
+            first_date_next_month = date(day=1, month=1, year=self.__calculated_month_date.year + 1)
+        else:
+            first_date_next_month = date(day=1, month=next_month, year=self.__calculated_month_date.year)
+
+        return (first_date_next_month - timedelta(days=1)).day
+
+    def get_selected_month_date(self):
+        qdate: QDate = self.month_date_edit.date()
+        return date(day=1, month=qdate.month(), year=qdate.year())
+
+    def disable_gui(self, disable: bool):
+        self.tool_bar_frame.setDisabled(disable)
+        self.main_content_frame.setDisabled(disable)
+
+    def set_status_bar_message(self, message: str):
+        self.status_bar_label.setText(message)
+
+    def plot_values(self, x_days_values: list, y_values: list):
+        self.plot_widget.getPlotItem().clear()
+        self.__set_left_axis_limits(y_range=(0, max(y_values)))
+        self.__draw_graph_lines(x_days_values, y_values)
+        self.__draw_graph_points(x_days_values, y_values)
+
+    def __set_left_axis_limits(self, y_range: tuple):
+        self.plot_widget.getPlotItem().getViewBox().setRange(
+            yRange=y_range
+        )
+
+    def __draw_graph_lines(self, month_axis: list, y_axis: list):
+        plot_curve_item = PlotCurveItem()
+        plot_curve_item.setData(month_axis, y_axis,
+                                pen=mkPen({'color': '#5599ff', 'width': 2}),
+                                antialias=True)
+        self.plot_widget.addItem(plot_curve_item)
+
+    def __draw_graph_points(self, month_axis: list, y_axis: list):
+        scatter_plot_item = ScatterPlotItem(size=14)
+        scatter_plot_item.setData(month_axis, y_axis,
+                                  symbol='o',
+                                  pen=mkPen({'color': '#5599ff'}),
+                                  brush=mkBrush('#5599ff'),
+                                  hoverBrush=mkBrush('#59DBFF'),
+                                  hoverable=True,
+                                  hoverSize=14)
+        self.plot_widget.addItem(scatter_plot_item)
