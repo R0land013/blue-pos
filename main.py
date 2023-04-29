69b915fc33a66041539de7b677377b32c822d85b
Rolando Rio <rolandorio06@gmail.com>
2023-04-29 11:14:48 -0400

    Style: Añadidos el nombre y el icono de la app para que el sistema operativo los reconozca y los muestre en la barra de tareas
    	Fue necesario actualizar la dependencia de easy-mvp a la versión 0.2.0

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from easy_mvp.intent import Intent
 
 from presenter.main import MainPresenter
 
-app = ApplicationManager()
+app = ApplicationManager(app_name='Blue POS',
+                         window_icon_path='./view/ui/images/bluepos.png')
 intent = Intent(MainPresenter)
 app.execute_app(intent)
