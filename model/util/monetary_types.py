8354643d7e3dc7e08498c1a1c588389d0986a1da
Javier Alejandro González Casellas <78095527+JalexCode@users.noreply.github.com>
2023-05-01 14:13:04 -0400

    Money error solving

--- model/util/monetary_types.py
+++ model/util/monetary_types.py
@@ -2,4 +2,6 @@ from money import Money
 
 
 def CUPMoney(amount: str) -> Money:
+    # changing "," by "." in amount string avoids Money error
+    amount = amount.replace(",", ".")
     return Money(amount=amount, currency='CUP')
