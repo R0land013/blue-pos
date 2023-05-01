3a720c235eea06cb4ffe8af895775b5a13089f74
Rolando Rio <rolandorio06@gmail.com>
2023-05-01 16:14:04 -0400

    No es necesario cambiar las comas en la función CUPMoney, la clase Money ya se asegura de eso

--- model/util/monetary_types.py
+++ model/util/monetary_types.py
@@ -2,6 +2,4 @@ from money import Money
 
 
 def CUPMoney(amount: str) -> Money:
-    # changing "," by "." in amount string avoids Money error
-    amount = amount.replace(",", ".")
     return Money(amount=amount, currency='CUP')
