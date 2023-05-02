b00c924fb2c032f3f44f84de81d0769d639fefe6
Rolando Rio <rolandorio06@gmail.com>
2023-05-02 11:05:39 -0400

    Fix: El CUPSpinBox usaba comas para separar la parte decimal en algunos sistemas operativos
            Se forzó que CUPSpinBox usara la localidad de USA para que así fuerce el uso de
            puntos en lugar de comas para separar la parte decimal, en cualquier SO.

--- view/util/cup_spin_box.py
+++ view/util/cup_spin_box.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QDoubleSpinBox
 from PyQt5.QtGui import QKeyEvent
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QLocale
 
 class CUPSpinBox(QDoubleSpinBox):
 
@@ -14,6 +14,11 @@ class CUPSpinBox(QDoubleSpinBox):
         self.setSuffix(' CUP')
         self.setDecimals(2)
         self.setSingleStep(10.00)
+        
+        # De este modo el separador entre la parte entera y decimal siempre será
+        # un punto, y no la coma. Aquí se fuerza porque puede variar dependiendo
+        # de la ubicación que tenga el Sistema Operativo
+        self.setLocale(QLocale(QLocale.Language.Spanish, QLocale.Country.UnitedStates))
 
     def keyPressEvent(self, event: QKeyEvent):
         if event.key() == Qt.Key.Key_Comma or event.key() == Qt.Key.Key_Plus:
